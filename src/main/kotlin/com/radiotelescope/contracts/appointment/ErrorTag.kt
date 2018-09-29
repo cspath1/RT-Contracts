9e5ee4b942c9cbdf1f40d9e685845f8294d6cdcc
Drevon <me@10.128.65.175>
2018-09-29 14:36:31 -0400

    Add error tag

---
+++ src/main/kotlin/com/radiotelescope/contracts/appointment/ErrorTag.kt
@@ -0,0 +1,17 @@
+package com.radiotelescope.contracts.appointment
+
+/**
+ * Field validation tags
+ */
+
+enum class ErrorTag{
+    USER_ID,
+    TYPE,
+    START_TIME,
+    END_TIME,
+    TELESCOPE_ID,
+    CELESTIAL_BODY_ID,
+    COORDINATES,
+    RECEIVER,
+    PUBLIC
+}
