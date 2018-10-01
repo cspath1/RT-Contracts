192639d0b6166ded11a917ef0a0c8341404de757
jmajeski <jmajeski@ycp.edu>
2018-10-01 16:37:51 -0400

    Remove responder interfaces #87

--- src/main/kotlin/com/radiotelescope/contracts/responder/Responder.kt
+++
@@ -1,8 +0,0 @@
-package.com.radiotelescope.contract.responder
-
-interface Responder<in SUCCESS_CALLBACK_TYPE, in FAILURE_CALLBACK_TYPE>
-{
-    fun onSuccess(t: SUCCESS_CALLBACK_TYPE)
-
-    fun onFailure(e: FAILURE_CALLBACK_TYPE)
-}
