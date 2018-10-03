8ef8b90b4c05b903d921601829beb3fc99143826
albatronix <jamoros@ycp.edu>
2018-10-03 05:07:36 -0400

    Fix imports, typos, fix return value, add to validation method (e.g. if startTime > endTime, error), fix Appointment return value, fix Appointment data class (add uFirstName, uLastName, date, orientation, celestialBody, change 'public' to 'isPublic' to avoid syntactical confusion, begin the SQL query for detecting clashes in Appointments already made

--- src/main/kotlin/com/radiotelescope/contracts/appointment/Create.kt
+++ src/main/kotlin/com/radiotelescope/contracts/appointment/Create.kt
@@ -3,71 +3,128 @@ package com.radiotelescope.contracts.appointment
 
 import com.radiotelescope.contracts.BaseCreateRequest
 import com.radiotelescope.contracts.Command
-import com.radiotelescope.repository.IUserRepository
-import com.radiotelescope.repository.User
-import com.radiotelescope.repository.IAppointmentRepository
-import com.radiotelescope.repository.Appointment
+import com.radiotelescope.repository.user.IUserRepository
+import com.radiotelescope.repository.user.User
+import com.radiotelescope.repository.appointment.IAppointmentRepository
+import com.radiotelescope.repository.appointment.Appointment
 import com.google.common.collect.HashMultimap
 import com.google.common.collect.Multimap
+import com.radiotelescope.contracts.SimpleResult
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 
+//create an appointment
 class Create(
     private val request: Request,
-    private val responder: CreateResponder<ErrorTag>,
     private val userRepo: IUserRepository,
     private val appointmentRepo: IAppointmentRepository
-) : Command<Long, Multimap<ErrorTag,String>>{
-    override fun execute(){
-        val errors = valideRequest()
+) : Command<Long, Multimap<ErrorTag,String>>
+{
+    override fun execute(): SimpleResult<Long, Multimap<ErrorTag, String>> {
+        val errors = validateRequest()
         if (!errors.isEmpty) {
             return SimpleResult(null, errors)
         } else {
             val newAppointment = appointmentRepo.save(request.toEntity())
-            return SimpleResult(appointment.id, null)
+            return SimpleResult(newAppointment.id, null)
         }
     }
 
-    private fun validateRequest(): Multimap<ErrorTag, String>{
-        val errors = HashMultimap.create<errorTag,String>()
+
+
+    interface getAppts
+    {
+        @Query("select startTime, endTime from appointment")
+        fun getTimesOfAppts():Appointment
+    }
+
+    private fun validateRequest(): Multimap<ErrorTag, String>
+    {
+        val errors = HashMultimap.create<ErrorTag,String>()
         with(request){
             /**
              * TODO : add validation checking
              */
-            if (startTime.isBlank())
+
+            if (startTime.toString().isBlank()) {
                 errors.put(ErrorTag.START_TIME, "Start time may not be blank")
-            if (endTime.isBlank())
+            }
+            if (endTime.toString().isBlank()) {
                 errors.put(ErrorTag.END_TIME, "End time may not be blank")
+            }
             if (receiver.isBlank())
                 errors.put(ErrorTag.RECEIVER, "Receiver may not be blank")
 
+            //If the start time > end time, we cause an error.
+            if (startTime.after(endTime))
+                errors.put(ErrorTag.START_TIME, "Start time may not be greater than end time")
+
+            //if that date scheduled is before the current date
+            if (date.before(Date()))
+                errors.put(ErrorTag.DATE, "Date of appointment may not be before current date" )
+
+
+
+            //if someone's trying to schedule an appointment that overlaps an existing scheduled appointment, cause an entry in the errors multimap
+            //iterate through all other appointments in the appointment table, and if there's a conflict... enter into errors map
+
+          /*  val aa:getAppts? = null
+            aa?.getTimesOfAppts()
+*/
+
+
+
+
+
+
+
+
+
+
         }
 
-        return errors
+      return errors
     }
 
 
     data class Request(
-            val userId: Long,
+            val user: User,
+    //        val orientation: Orientation, //to implement-- uncomment once implemented
+    //        val celestialBody: CelestialBody,
             val type: String,
             val startTime: Date,
             val endTime: Date,
-            val telescopeId: Int,
-            val celestialBodyId: Int,
-            val coordinates: Int,
+            val telescopeId: Long,
+            val celestialBodyId: Long,
             val receiver: String,
-            val public: Boolean
+            val isPublic: Boolean,
+            val date: Date, //date being the day/month/year of the appt?
+            val assocUserId: Int,
+            val uFirstName: String,
+            val uLastName: String,
+            val apptId: Long,
+            val status: Appointment.Status,
+            val state: Int
     ) : BaseCreateRequest<Appointment>{
         override fun toEntity(): Appointment {
             return Appointment(
-                    userId = userId,
+                    user= user,
+         //           orientation = orientation,  //uncomment once implemented
+         //           celestialBody = celestialBody,
                     type = type,
                     startTime = startTime,
                     endTime = endTime,
-                    telescopeid = telescopeId,
+                    telescopeId = telescopeId,
                     celestialBodyId = celestialBodyId,
-                    coordinates = coordinates,
                     receiver = receiver,
-                    public = public
+                    isPublic = isPublic,
+                    date = date,
+                    assocUserId = assocUserId,
+                    uFirstName = uFirstName,
+                    uLastName = uLastName,
+                    //Status = status.Scheduled, //should be set to 'Scheduled' ?
+                    state = state
             )
         }
     }
