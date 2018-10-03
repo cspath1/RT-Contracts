ac4b2bc683d2e9161fa322010b4381f7f4e1c3eb
albatronix <jamoros@ycp.edu>
2018-10-03 10:37:14 -0400

    Fix AppointmentTest

--- src/test/kotlin/com/radiotelescope/repository/appointment/AppointmentTest.kt
+++ src/test/kotlin/com/radiotelescope/repository/appointment/AppointmentTest.kt
@@ -1,5 +1,6 @@
 package com.radiotelescope.repository.appointment
 
+import com.radiotelescope.repository.user.User
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -7,6 +8,7 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.junit4.SpringRunner
+import java.util.*
 
 
 @DataJpaTest
@@ -17,26 +19,46 @@ internal class AppointmentTest {
     @Autowired
     private lateinit var appointmentRepo: IAppointmentRepository
 
-    private var userId: Long?
+    private var userId: Int = -1
+    var u: User = User("Someone", "LastName123", "piano1mano@gmail.com","123456" )
+
+    val d =  Date()
+    val dd = Date("2018-1-1")
+    // Instantiate and persist an Appointment Entity Object
+    var a:Appointment = Appointment(u, "appt-type1", d, dd, 2, 4, "1", true, Date(), 1, u.firstName, u.lastName, 0 )
 
     @Before
     fun setUp() {
-        // Instantiate and persist an Appointment Entity Object
-        val appointment = Appointment(1,"type", Date(100),Date(200),1,2,100,"receiver",true)
-        appointmentRepo.save(appointment)
-
-        userId = appointment.userId
+        appointmentRepo.save(a)
+        userId = a.assocUserId
     }
 
     @Test
     fun testFindByUserId() {
 
-
         // Use the variable set in the set up method
-        val userAppointment: List<Appointment> = appointmentRepo.findByUserId(UserId).first()
+        //Get all the appointments by this user
 
-        // The found appointment should have a user id of 1
-        Assert.assertTrue(userAppointment.userId = 1)
+        val appts: List<Appointment> = appointmentRepo.findByUser(u)
+
+        for (aa:Appointment in appts) {
+
+            if (aa.assocUserId == 1)
+            {
+                Assert.assertEquals(aa.type, a.type)
+                Assert.assertEquals(aa.user, a.user)
+                Assert.assertEquals(aa.startTime, a.startTime)
+                Assert.assertEquals(aa.endTime, a.endTime)
+                Assert.assertEquals(aa.telescopeId, a.telescopeId)
+                Assert.assertEquals(aa.celestialBodyId, a.celestialBodyId)
+                Assert.assertEquals(aa.receiver, a.receiver)
+                Assert.assertEquals(aa.isPublic, a.isPublic)
+                Assert.assertEquals(aa.date, a.date)
+                Assert.assertEquals(aa.assocUserId, a.assocUserId)
+                Assert.assertEquals(aa.uFirstName, a.uLastName)
+                Assert.assertEquals(aa.state, a.state)
+            }
+        }
+
     }
-}
 }
