733f237b4461e93dab7c2099659dcbe8b9456aa0
cody <cspath1@ycp.edu>
2018-10-03 14:33:43 -0400

    Implement AppointmentRepository method needed for finding the future list of appointments
    for a given user

--- src/main/kotlin/com/radiotelescope/repository/appointment/IAppointmentRepository.kt
+++ src/main/kotlin/com/radiotelescope/repository/appointment/IAppointmentRepository.kt
@@ -1,6 +1,9 @@
 package com.radiotelescope.repository.appointment;
 
 import com.radiotelescope.repository.user.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
@@ -14,26 +17,16 @@ find appointment by userId
 
  */
 
+    @Query(value = "SELECT * " +
+            "FROM appointment " +
+            "WHERE user_id=?1 AND end_time > CURRENT_TIMESTAMP()",
+            countQuery = "SELECT COUNT(*) " +
+                    "FROM appointment " +
+                    "WHERE user_id=?1 AND end_time > CURRENT_TIMESTAMP() \\n#pageable\\n",
+            nativeQuery = true)
+    fun findFutureAppointmentsByUser(userId: Long, pageable: Pageable): Page<Appointment>
 
     fun findByUser(user: User): List<Appointment>
-
-   //May need to implement
- //   fun findByUserId(uId: Long): List<Appointment>
-
-    //needs to be implemented
-  //  fun findByUserFirstAndLastName(): List<Appointment>
-
-    //needs to be implemented
-  //  fun existsByUserFirstAndLastName(): Boolean
-
-    //needs to be implemented. (if one appt exists, by a Us
-    fun existsByUserId(): Boolean
-
-    //a specific appointment with a particular appointment ID (findById in-built)
- //   fun findByApptId(apptId : Long): Appointment
-
-
-
 
     override fun delete(a: Appointment): Unit
     {
