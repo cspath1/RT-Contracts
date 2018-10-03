a9bcd9dea3750baf60149054530eed23a2be53a0
albatronix <jamoros@ycp.edu>
2018-10-03 08:13:04 -0400

    Add user, uLastName, uFirstName, state, date, assocUserId, isPublic, comment out (for now) possible params orientation, celestialBody,

--- src/main/kotlin/com/radiotelescope/repository/appointment/Appointment.kt
+++ src/main/kotlin/com/radiotelescope/repository/appointment/Appointment.kt
@@ -1,6 +1,9 @@
 package com.radiotelescope.repository.appointment
 
+import com.radiotelescope.repository.user.User
+import java.util.*
 import javax.persistence.*
+
 
 /**
  * Entity Class representing an Appointment for the web-application
@@ -10,9 +13,16 @@ import javax.persistence.*
 @Entity
 @Table(name = "appointment")
 data class Appointment(
-        @Column(name = "user_id", nullable = false)
-        var userId: Long,
+        @ManyToOne
+        @JoinColumn(name = "user_id", nullable = false)
+        var user: User,
         @Column(name = "type", nullable = false)
+  //      @OneToMany //because one appointment will have many latitudes and longitudes (i.e. azimuth and elevation)
+  //       @JoinColumn(name="orientation_id", nullable= false)
+  //      var orientation: Orientation, //yet to implement
+  //      @OneToMany //one appointment could be looking at more than one celestial body?
+  //      @JoinColumn(name="celestialBody_id", nullable = false)
+  //      var celestialBody: CelestialBody, //same
         var type: String,
         @Column(name = "start_time", nullable = false, unique = true)
         var startTime: Date,
@@ -22,12 +32,24 @@ data class Appointment(
         var telescopeId: Long,
         @Column(name = "celestial_body_id", nullable = false)
         var celestialBodyId: Long,
-        @Column(name = "coordinates", nullable = false)
-        var coordinates: Int,
+  //      @Column(name = "coordinates", nullable = false) //unnecessary if we have orientation
+   //     var coordinates: Int,
         @Column(name = "receiver", nullable = false)
         var receiver: String,
-        @Column(name = "public", nullable = false)
-        var public: Boolean
+        @Column(name = "isPublic", nullable = false)
+        var isPublic: Boolean,
+        @Column(name = "date", nullable = false)
+        var date: Date,
+        @Column(name = "assocUserId", nullable= false)
+        var assocUserId: Int,
+        @Column(name = "uFirstName", nullable= false)
+        var uFirstName: String,
+        @Column(name = "uLastName", nullable = false)
+        var uLastName: String,
+        @Column(name = "state", nullable = false)
+        var state: Int
+
+
 
 ) {
     @Id
