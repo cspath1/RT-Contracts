42c5c49763e5cc00ba15a2decb6186855e3ab4a0
albatronix <jamoros@ycp.edu>
2018-10-03 08:00:20 -0400

    Add retrieve, retrieveList, create, delete

--- src/main/kotlin/com/radiotelescope/contracts/appointment/BaseAppointmentFactory.kt
+++ src/main/kotlin/com/radiotelescope/contracts/appointment/BaseAppointmentFactory.kt
@@ -1,41 +1,58 @@
 package com.example.project.contracts.appointment
 
-import com.radiotelescope.contract.Command
-import com.radiotelescope.contract.responder.CreateResponder
-import com.radiotelescope.contract.responder.DeleteResponder
-import com.radiotelescope.contract.responder.RetrieveResponder
-import com.radiotelescope.contract.responder.UpdateResponder
+import com.google.common.collect.Multimap
+import com.radiotelescope.contracts.Command
+import com.radiotelescope.contracts.appointment.*
+
 import com.radiotelescope.repository.appointment.IAppointmentRepository
+import com.radiotelescope.repository.user.IUserRepository
+import com.radiotelescope.repository.user.User
+
 
 class BaseAppointmentFactory(
-    private val apptRepo: AppointmentRepository
+    private val apptRepo: IAppointmentRepository,
+    private var apptInfo: AppointmentInfo,
+   private var userRepo: IUserRepository //Would it make sense for the BaseApptFactory to have a userRepo?
+        // (for the function to find appts by a User, fun findByUser(): List<Appointment>, in IUserRepository,
+//findByUser is called in RetrieveList
 
 
-) : AppointmentFactory {
-    override fun retrieve(id: Long, responder: RetrieveResponder<AppointmentInfo, ErrorTag>): Command {
-        return Retrieve(id, responder, apptRepo)
+): AppointmentFactory {
 
+    //gets an appointment by id
+    override fun retrieve(id: Long): Command<Long, Multimap<ErrorTag, String>> {
+        return Retrieve(apptRepo.findById(id).get(), apptInfo , apptRepo, id)
     }
 
-    override fun retrieve(request: Validate.Request, responder: RetrieveResponder<AppointmentInfo, ErrorTag>): Command {
+    override fun retrieveList(u: User):Command<Long,Multimap<ErrorTag, String>>
+    {
+        return RetrieveList(apptRepo, u, userRepo)
+    }
 
-        return Validate(request, responder, apptRepo)
+
+    //implement
+    /*
+    override fun retrieve(request: Validate.Request): Command<Long, Multimap<ErrorTag, String>>  {
 
 
     }
+    */
 
-    override fun create(request: Create.Request, responder: CreateResponder<ErrorTag>): Command {
+    //create an appointment
+    override fun create(request: Create.Request): Command<Long, Multimap<ErrorTag, String>>  {
 
-        return Create(request, responder, apptRepo)
-
+        return Create(request, apptRepo)
     }
 
-    override fun update(request: Update.Request, responder: UpdateResponder<ErrorTag>): Command {
-        return Update(request, responder, apptRepo)
+    /*
+    override fun update(request: Update.Request): Command<Long, Multimap<ErrorTag, String>>  {
+        return Update(request, apptRepo)
     }
+    */
 
-    override fun delete(id: Long, responder: DeleteResponder<ErrorTag>): Command {
-        return Delete(id, responder, apptRepo)
+    //delete an appointment
+    override fun delete(id: Long): Command<Long, Multimap<ErrorTag, String>>  {
+        return Delete(apptRepo.findById(id).get(), apptRepo)
     }
 
 }
