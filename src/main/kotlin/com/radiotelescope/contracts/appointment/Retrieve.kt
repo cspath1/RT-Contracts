409d72594109cbdfedb1a488c4ccfe448e518499
albatronix <jamoros@ycp.edu>
2018-10-07 19:42:11 -0400

    Use secondary constructor, remove save call (unnecessary for Retrieve)

--- src/main/kotlin/com/radiotelescope/contracts/appointment/Retrieve.kt
+++ src/main/kotlin/com/radiotelescope/contracts/appointment/Retrieve.kt
@@ -1,65 +1,32 @@
 package com.radiotelescope.contracts.appointment
 
-import com.radiotelescope.contracts.BaseCreateRequest
 import com.radiotelescope.contracts.Command
-import com.radiotelescope.repository.user.IUserRepository
-import com.radiotelescope.repository.user.User
 import com.radiotelescope.repository.appointment.IAppointmentRepository
 import com.radiotelescope.repository.appointment.Appointment
 import com.google.common.collect.HashMultimap
 import com.google.common.collect.Multimap
 import com.radiotelescope.contracts.SimpleResult
-import org.springframework.data.jpa.repository.Query
-import java.util.*
 
-//To get Appointment(s) from the db
-
+//This class retrieves one specific appointment, by its appointment ID.
 class Retrieve(
-   private var a: Appointment,
-   private var aI: AppointmentInfo,
-   private val aRepo: IAppointmentRepository,
-   private val aId: Long
-
+   private var appt: Appointment,
+   private var apptInfo: AppointmentInfo,
+   private val apptRepo: IAppointmentRepository,
+   private val appt_id: Long
 ): Command<Long, Multimap<ErrorTag,String>>
 {
-    override fun execute(): SimpleResult<Long, Multimap<ErrorTag, String>> {
-
+        override fun execute(): SimpleResult<Long, Multimap<ErrorTag, String>> {
         val errors = HashMultimap.create<ErrorTag, String>()
-
-        //retrieve one appt by aId, retrieve all appts by uId
-
-        //if we're trying to get an appt that does not exist
-
-
-        if (!aRepo.existsById(aId)) {
-            errors.put(ErrorTag.ID, "Attempted to retrieve appointment with id $aId that does not exist")
-            //failure
+        //Failure case
+        if (!apptRepo.existsById(appt_id)) {
+            errors.put(ErrorTag.ID, "Attempted to retrieve appointment with id $appt_id that does not exist")
             return SimpleResult(null, errors)
         }
-        //if it does exist
+        //Success case
         else {
-            a = aRepo.findById(aId).get()
-            //See if I can get secondary constructor here (just pass in appt)
-            aI = AppointmentInfo(a, a.user, a.type, a.startTime, a.endTime, a.telescopeId, a.celestialBodyId, a.receiver, a.isPublic, a.date, a.assocUserId, a.uFirstName,
-                    a.uLastName, a.id, a.status, a.state)
-
-            //Is save the correct method to use for a retrieve?
-            aRepo.save(a)
-
-            //success
-            return SimpleResult(a.id, null)
+            appt = apptRepo.findById(appt_id).get()
+            apptInfo = AppointmentInfo(appt)
+            return SimpleResult(appt_id, null)
         }
-
-
-
-
-
-
-
-
-
     }
-
-
-
 }
