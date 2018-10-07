406bd4df39c158e520a485393b623c84b8ce488e
cody <cspath1@ycp.edu>
2018-10-07 00:18:24 -0400

    Add javadocs to new role contracts

--- src/main/kotlin/com/radiotelescope/contracts/role/ErrorTag.kt
+++ src/main/kotlin/com/radiotelescope/contracts/role/ErrorTag.kt
@@ -1,5 +1,8 @@
 package com.radiotelescope.contracts.role
 
+/**
+ * Enum representing field validation errors
+ */
 enum class ErrorTag {
     ID,
     USER_ID,
--- src/main/kotlin/com/radiotelescope/contracts/role/UnapprovedList.kt
+++ src/main/kotlin/com/radiotelescope/contracts/role/UnapprovedList.kt
@@ -5,16 +5,29 @@ import com.radiotelescope.contracts.Command
 import com.radiotelescope.contracts.SimpleResult
 import com.radiotelescope.contracts.user.UserInfo
 import com.radiotelescope.repository.role.IUserRoleRepository
+import com.radiotelescope.repository.role.UserRole
 import com.radiotelescope.repository.user.IUserRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 
+/**
+ * Override of the [Command] interface used to approve a [UserRole]
+ *
+ * @param pageable the [Pageable] request
+ * @param userRepo the [IUserRepository] interface
+ * @param userRoleRepo the [IUserRoleRepository] interface
+ */
 class UnapprovedList(
         private val pageable: Pageable,
         private val userRepo: IUserRepository,
         private val userRoleRepo: IUserRoleRepository
 ) : Command<Page<UserRoleInfo>, Multimap<ErrorTag, String>> {
+    /**
+     * Override of the [Command.execute] method that will grab any [UserRole] objects
+     * that need approved by an admin and adapts them into a [UserRoleInfo] before
+     * returning a [Page] to the client.
+     */
     override fun execute(): SimpleResult<Page<UserRoleInfo>, Multimap<ErrorTag, String>> {
         val unapprovedRoles = userRoleRepo.findNeedsApprovedUserRoles(pageable)
 
