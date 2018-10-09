06c961b931e3a5e10f6b808af086aab462f6760c
jmajeski <jmajeski@ycp.edu>
2018-10-09 11:51:28 -0400

    add findPastAppointmentByUser for retrieving lists of appointments

--- src/main/kotlin/com/radiotelescope/contracts/appointment/RetrieveList.kt
+++ src/main/kotlin/com/radiotelescope/contracts/appointment/RetrieveList.kt
@@ -7,23 +7,26 @@ import com.radiotelescope.repository.appointment.Appointment
 import com.google.common.collect.HashMultimap
 import com.google.common.collect.Multimap
 import com.radiotelescope.contracts.SimpleResult
+import org.springframework.data.domain.Pageable
 
 //Only to be for past appointments
 
 class RetrieveList(
        private val apptRepo: IAppointmentRepository,
-        private val userId:Long,
-        private val userRepo: IUserRepository
+       private val userId:Long,
+       private val userRepo: IUserRepository,
+       private val pageable: Pageable
 ):Command <Long, Multimap<ErrorTag, String>> {
     override fun execute(): SimpleResult<Long, Multimap<ErrorTag, String>> {
         val errors = HashMultimap.create<ErrorTag, String>()
+
         //check if the user exists
         if (!userRepo.existsById(userId)) {
             errors.put(ErrorTag.USER_ID, "User with id ${userId} does not exist")
             return SimpleResult(userId, errors)
             //Success case
         } else {
-           val apptList = userRepo.findByUser()
+           val apptList = apptRepo.findPastAppointmentsByUser(userId, pageable)
             for (appt: Appointment in apptList) {
                 val apptInfo = AppointmentInfo(appt)
                 val apptSaved : Appointment = apptRepo.save(appt)
