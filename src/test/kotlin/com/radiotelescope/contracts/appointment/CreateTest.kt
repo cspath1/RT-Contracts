5739b5e72edd0e323d7b1a36c04f88838382d185
albatronix <jamoros@ycp.edu>
2018-10-10 06:13:58 -0400

    Test Create.execute in CreateTest

--- src/test/kotlin/com/radiotelescope/contracts/appointment/CreateTest.kt
+++ src/test/kotlin/com/radiotelescope/contracts/appointment/CreateTest.kt
@@ -14,48 +14,31 @@ import org.springframework.test.context.junit4.SpringRunner
 import java.util.*
 import com.google.common.collect.Multimap
 import com.radiotelescope.contracts.SimpleResult
+import org.springframework.beans.factory.annotation.Autowired
 
 @DataJpaTest
 @RunWith(SpringRunner::class)
 internal class CreateTest
 {
-
+    @Autowired
     private lateinit var apptRepo: IAppointmentRepository
-    private lateinit var appt: Appointment
     var u:User= User("Someone", "LastName123", "piano1mano@gmail.com","123456" )
-    var request:Request  = Create.Request(u, "type1", Date(), Date("2018-7-7"), 1, 2, "1", true, Date(), 3, "John", "Doe", 1, Appointment.Status.InProgress, 1 )
-    var Createt:Create = Create(request, apptRepo)
+    var request:Request  = Create.Request(u, "type1", Date(), Date("2019-7-7"), 1, 2, "1", true, 500, u.firstName, u.lastName, 500,  Appointment.Status.InProgress, 1 )
+    var CreateObj:Create = Create(request, apptRepo)
 
     @Before
     fun init() {
-
-
     }
-
 
     @Test
     fun CreateTest()
     {
-      var errors = HashMultimap.create<ErrorTag,String>()
-
-     var s: SimpleResult<Long, Multimap<ErrorTag, String>> =  SimpleResult(null, errors)
-        s =  Createt.execute()
-
-      //  if (!errors.isEmpty)
-
-
-        //pass if errors Multimap remains empty
-          if ( errors.isEmpty() )
-          {
-
-          }
-        //fail if otherwise
-        else assertTrue(false)
-
-
+        var errors = HashMultimap.create<ErrorTag,String>()
+        var s: SimpleResult<Long, Multimap<ErrorTag, String>> =  SimpleResult(null, errors)
+        s =  CreateObj.execute()
+        //fail
+        if (s.success == null)
+            return assertTrue(false)
+        //else pass
     }
-
-
-
-
 }
