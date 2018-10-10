9790ee58b251bafe7a8a88c9039d422182a838bf
albatronix <jamoros@ycp.edu>
2018-10-10 06:18:46 -0400

    Test Delete.execute in DeleteTest

--- src/test/kotlin/com/radiotelescope/contracts/appointment/DeleteTest.kt
+++ src/test/kotlin/com/radiotelescope/contracts/appointment/DeleteTest.kt
@@ -4,39 +4,34 @@ import com.google.common.collect.HashMultimap
 import com.radiotelescope.repository.appointment.IAppointmentRepository
 import com.radiotelescope.repository.appointment.Appointment
 import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.junit4.SpringRunner
 import com.google.common.collect.Multimap
 import com.radiotelescope.contracts.SimpleResult
+import com.radiotelescope.repository.user.User
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
 
 @DataJpaTest
 @RunWith(SpringRunner::class)
 internal class DeleteTest
 {
-
+    @Autowired
     private lateinit var apptRepo: IAppointmentRepository
-    private lateinit var appt: Appointment
-    var deleteT:Delete = Delete(appt, apptRepo)
-
+    var u: User = User("Someone", "LastName123", "piano1mano@gmail.com","123456" )
+    var request: Create.Request = Create.Request(u, "type1", Date(), Date("2019-7-7"), 1, 2, "1", true, 500, u.firstName, u.lastName, 500,  Appointment.Status.InProgress, 1 )
+    var deleteObj:Delete = Delete(request.apptId, apptRepo)
 
     @Test
-fun DeleteTest()
+    fun deleteTest()
     {
         var errors = HashMultimap.create<ErrorTag,String>()
-        var s: SimpleResult<Long, Multimap<ErrorTag, String>> =  SimpleResult(null, errors)
-
-        s =  deleteT.execute()
-
-        //pass if errors Multimap remains empty
-        if ( errors.isEmpty() )
-        {
-
-        }
-        //fail if otherwise
-        else assertTrue(false)
-
+        var s: SimpleResult<Long, Multimap<ErrorTag, String>> = deleteObj.execute()
+        //fail case
+        if (s.success== null)
+            assertTrue(false)
+        //else pass
     }
 }
