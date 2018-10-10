aa2749219994101a88744d1a92dc9d891b4cba3a
albatronix <jamoros@ycp.edu>
2018-10-10 06:20:42 -0400

    Test Retrieve.execute in RetrieveTest

--- src/test/kotlin/com/radiotelescope/contracts/appointment/RetrieveTest.kt
+++ src/test/kotlin/com/radiotelescope/contracts/appointment/RetrieveTest.kt
@@ -10,7 +10,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.junit4.SpringRunner
 import com.google.common.collect.Multimap
 import com.radiotelescope.contracts.SimpleResult
+import com.radiotelescope.repository.appointment.IAppointmentRepository
+import com.radiotelescope.repository.user.IUserRepository
 import com.radiotelescope.repository.user.User
+import org.springframework.beans.factory.annotation.Autowired
 import java.util.*
 
 @DataJpaTest
@@ -20,38 +23,22 @@ internal class RetrieveTest
 {
 
     private var u: User = User("Someone", "LastName123", "piano1mano@gmail.com","123456" )
-    val d =  Date()
-    val dd = Date("2018-1-1")
-
-    private var a:Appointment = Appointment(u, "appt-type1", d, dd, 2, 4, "1", true, Date(), 3, u.firstName, u.lastName, 0 )
-
-    private var aI: AppointmentInfo = AppointmentInfo(a, a.user, a.type, a.startTime, a.endTime, a.telescopeId, a.celestialBodyId, a.receiver, a.isPublic, a.date, a.assocUserId, a.uFirstName,
-    a.uLastName, a.id, a.status, a.state)
-
-
-    //How do you initialize a Repository object?
-    //if I need to pass in a repository object to Retrieve?
-
-  //  private val aRepo: IAppointmentRepository
-
-    private val retrieveT:Retrieve = Retrieve(a, aI, aRepo, a.id)
+    val startDate =  Date()
+    val endDate = Date("2018-1-1")
+    private var a:Appointment = Appointment(u, "appt-type1", startDate, endDate, 2, 4, "1", true, 500, u.firstName, u.lastName, 5 )
+    private var apptInfo: AppointmentInfo = AppointmentInfo(a)
+    @Autowired
+    private lateinit var apptRepo: IAppointmentRepository
+    private val retrieveObj:Retrieve = Retrieve(a, apptInfo, apptRepo, a.id)
 
     @Test
 fun RetrieveTest()
     {
         var errors = HashMultimap.create<ErrorTag,String>()
-        var s: SimpleResult<Long, Multimap<ErrorTag, String>> =  SimpleResult(null, errors)
-
-        s =  retrieveT.execute()
-
-        //pass if errors Multimap remains empty
-        if ( errors.isEmpty() )
-        {
-
-        }
-        //fail if otherwise
-        else assertTrue(false)
-
-
+        var s: SimpleResult<Long, Multimap<ErrorTag, String>> =  retrieveObj.execute()
+        //fail case
+        if ( s.success == null )
+         assertTrue(false)
+//else pass
     }
 }
