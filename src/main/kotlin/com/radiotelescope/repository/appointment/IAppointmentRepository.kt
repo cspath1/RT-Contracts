1344791f07c800f8cc4479c2605171377780941f
albatronix <jamoros@ycp.edu>
2018-10-16 23:19:18 -0400

    (no change)

--- src/main/kotlin/com/radiotelescope/repository/appointment/IAppointmentRepository.kt
+++ src/main/kotlin/com/radiotelescope/repository/appointment/IAppointmentRepository.kt
@@ -1,12 +1,10 @@
 package com.radiotelescope.repository.appointment
 
-import com.radiotelescope.repository.user.User
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 /*
 Spring Repository Interface for the Appointment Entity
