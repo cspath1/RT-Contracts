20a3fe464e961190e9a09a1e0b366eb4dcda905e
albatronix <jamoros@ycp.edu>
2018-10-17 00:10:00 -0400

    Put one param on one line

--- src/main/kotlin/com/radiotelescope/repository/appointment/IAppointmentRepository.kt
+++ src/main/kotlin/com/radiotelescope/repository/appointment/IAppointmentRepository.kt
@@ -22,7 +22,9 @@ interface IAppointmentRepository : PagingAndSortingRepository<Appointment, Long>
             nativeQuery = true)
     fun findFutureAppointmentsByUser(userId: Long, pageable: Pageable): Page<Appointment>
 
-    @Query(value = "select * from appointment where user_id=?1 AND end_time < CURRENT_TIMESTAMP", countQuery = "select count(*) from appointment where user_id=?1 and end_time < current_timestamp", nativeQuery = true)
+    @Query(value = "select * from appointment where user_id=?1 AND end_time < CURRENT_TIMESTAMP",
+            countQuery = "select count(*) from appointment where user_id=?1 and end_time < current_timestamp",
+            nativeQuery = true)
     fun findPreviousAppointmentsByUser(userId: Long, pageable:Pageable): Page<Appointment>
 
     @Query(value ="select * from appointment where telescope_id = ?1", nativeQuery = true)
