bc75ee3ae62680fbf24410cf07e4d69a3142ec32
albatronix <jamoros@ycp.edu>
2018-10-16 22:55:32 -0400

    Add logic to prevent canceling an already canceled appointment

--- src/main/kotlin/com/radiotelescope/contracts/appointment/Cancel.kt
+++ src/main/kotlin/com/radiotelescope/contracts/appointment/Cancel.kt
@@ -15,16 +15,24 @@ class Cancel
 {
     override fun execute(): SimpleResult<Long, Multimap<ErrorTag, String>> {
         //Failure case: Appointment does not exist
-        if (!apptRepo.existsById(apptId)) {
-            val errors = HashMultimap.create<ErrorTag, String>()
-            errors.put(ErrorTag.ID, "appt with ID ${apptId} does not exist (attempted deletion)")
+        val errors = HashMultimap.create<ErrorTag, String>()
+        if (!apptRepo.existsById(apptId))
+        {
+            errors.put(ErrorTag.ID, "appt with ID ${apptId} does not exist (attempted cancellation)")
             return SimpleResult(null, errors)
         }
-        //Success case: Found appointment to delete
+        //Success case: Found appointment to cancel
         else
         {
             val appt:Appointment = apptRepo.findById(apptId).get()
+
+            if (appt.status != Appointment.Status.Canceled)
             appt.status = Appointment.Status.Canceled
+            else {
+                errors.put(ErrorTag.STATUS, "Cannot cancel an already canceled appointment: appointment id is ${apptId}")
+                return SimpleResult(null, errors)
+            }
+
             apptRepo.save(appt)
             return SimpleResult(apptId, null)
         }
