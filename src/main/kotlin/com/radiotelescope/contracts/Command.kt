6a5a7e2d9810ec2b30f407e4ad0be0aa35002969
cody <cspath1@ycp.edu>
2018-10-16 22:02:15 -0400

    Add more javadocs to Command interface

--- src/main/kotlin/com/radiotelescope/contracts/Command.kt
+++ src/main/kotlin/com/radiotelescope/contracts/Command.kt
@@ -4,7 +4,10 @@ import com.radiotelescope.security.AccessReport
 
 /**
  * Command Interface responsible for handling all interactions
- * with the Spring Data JPA layer of the application
+ * with the Spring Data JPA layer of the application.
+ *
+ * The S type parameter refers to whatever the success data type is
+ * The E type parameter refers to whatever the error data type is
  */
 interface Command<out S, out E> {
     /**
@@ -14,7 +17,11 @@ interface Command<out S, out E> {
 }
 
 /**
- * Command Interface responsible for handling all user authentication
+ * Command Interface responsible for handling all user authentication.
+ *
+ * The S type parameter refers to whatever the success data type is
+ * The E type parameter refers to whatever the error data type is
+ *
  * @param withAccess an anonymous function that takes a [SimpleResult] as a parameter and returns [Unit]
  */
 interface SecuredAction<out S, out E> {
