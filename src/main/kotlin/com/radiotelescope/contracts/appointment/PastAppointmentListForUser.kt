8dfcd2358b4344431e6a939e98119cbc4d4a95b6
jmajeski <jmajeski@ycp.edu>
2018-10-19 10:23:57 -0400

    Add past appointment list for user documentation

--- src/main/kotlin/com/radiotelescope/contracts/appointment/PastAppointmentListForUser.kt
+++ src/main/kotlin/com/radiotelescope/contracts/appointment/PastAppointmentListForUser.kt
@@ -12,7 +12,15 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 
-//Only for PAST appointments
+/**
+ * Override of the [Command] interface used to retrieve a User's
+ * future appointments.
+ * @param apptRepo the [IAppointmentRepository] interface
+ * @param userId the user ID
+ * @param userRepo the [IUserRepository] interface
+ * @param pageRequest the [pageRequest] interface
+
+ */
 
 class PastAppointmentListForUser(
         private val apptRepo: IAppointmentRepository,
@@ -20,6 +28,15 @@ class PastAppointmentListForUser(
         private val userRepo: IUserRepository,
         private val pageRequest: PageRequest
         ):Command <Page<AppointmentInfo>, Multimap<ErrorTag, String>> {
+    /**
+     * Override of the [Command] execute method. Checks if the user exists by user id.
+     *
+     * If user exists it will create a [Page] of [AppointmentInfo] objects and
+     * return this in the [SimpleResult.success] value.
+     *
+     * If user does not exist, it will will return the errors in a [SimpleResult.error]
+     * value with a null success.
+     */
     override fun execute(): SimpleResult<Page<AppointmentInfo>, Multimap<ErrorTag, String>> {
         val errors = HashMultimap.create<ErrorTag, String>()
 
@@ -31,9 +48,10 @@ class PastAppointmentListForUser(
         } else {
            val apptList = apptRepo.findPreviousAppointmentsByUser(userId, pageRequest)
             val infoPage = apptList.toAppointmentInfoPage()
-            for (appt: Appointment in apptList) {
-                val apptInfo = AppointmentInfo(appt)
-            }
+            //unused
+//            for (appt: Appointment in apptList) {
+//                val apptInfo = AppointmentInfo(appt)
+//            }
             return SimpleResult(infoPage, null)
         }
     }
