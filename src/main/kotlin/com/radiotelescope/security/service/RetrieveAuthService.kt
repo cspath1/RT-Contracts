16e07fadd39052a315af88240f25b8b2f135118c
cody <cspath1@ycp.edu>
2018-10-29 23:53:55 -0400

    Modify RetrieveAuthService to return errors if the user is not active

--- src/main/kotlin/com/radiotelescope/security/service/RetrieveAuthService.kt
+++ src/main/kotlin/com/radiotelescope/security/service/RetrieveAuthService.kt
@@ -5,6 +5,7 @@ import com.google.common.collect.Multimap
 import com.radiotelescope.contracts.SimpleResult
 import com.radiotelescope.contracts.user.ErrorTag
 import com.radiotelescope.repository.user.IUserRepository
+import com.radiotelescope.repository.user.User
 import com.radiotelescope.security.AuthenticatedUserToken
 import com.radiotelescope.security.UserSession
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -45,6 +46,8 @@ class RetrieveAuthService(
 
         val user = userRepo.findById(authToken.userId!!).get()
 
+        validateStatus(user.status)?.let { return SimpleResult(null, it) }
+
         val userSession = UserSession(
                 userId = user.id,
                 email = user.email,
@@ -54,5 +57,25 @@ class RetrieveAuthService(
         )
 
         return SimpleResult(userSession, null)
+    }
+
+    /**
+     * Since a user should not be validated unless they are active,
+     * this method checks the user's status to see if they are active
+     * or not, and creates errors accordingly
+     *
+     * @param status the [User.Status]
+     * @return a [Multimap] of errors or null
+     */
+    fun validateStatus(status: User.Status): Multimap<ErrorTag, String>? {
+        val errors = HashMultimap.create<ErrorTag, String>()
+        when (status) {
+            User.Status.Deleted -> errors.put(ErrorTag.STATUS, "User was not found")
+            User.Status.Banned -> errors.put(ErrorTag.STATUS, "User is currently banned")
+            User.Status.Inactive -> errors.put(ErrorTag.STATUS, "User is not currently activated")
+            User.Status.Active -> return null
+        }
+
+        return errors
     }
 }
