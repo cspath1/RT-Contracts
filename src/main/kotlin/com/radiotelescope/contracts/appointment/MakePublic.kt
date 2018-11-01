f244b232e24b2d5069a8eeb8097e5a3d927006bf
rpim <rpim@ycp.edu>
2018-10-31 23:52:42 -0400

    Implement MakePublic command object

---
+++ src/main/kotlin/com/radiotelescope/contracts/appointment/MakePublic.kt
@@ -0,0 +1,57 @@
+package com.radiotelescope.contracts.appointment
+
+import com.google.common.collect.HashMultimap
+import com.google.common.collect.Multimap
+import com.radiotelescope.contracts.Command
+import com.radiotelescope.contracts.SimpleResult
+import com.radiotelescope.repository.appointment.Appointment
+import com.radiotelescope.repository.appointment.IAppointmentRepository
+
+/**
+ * Command class for editing an appointment
+ *
+ * @param appointmentId the appointment Id
+ * @param appointmentRepo of type [IAppointmentRepository]
+ */
+class MakePublic (
+        private val appointmentId: Long,
+        private val appointmentRepo: IAppointmentRepository
+): Command<Long, Multimap<ErrorTag, String>> {
+    /**
+     * Override of the [Command.execute] method. Calls the [validateRequest] method
+     * that will handle all constraint checking and validation.
+     *
+     * If validation passes, it will update and persist the [Appointment] object and
+     * return the id in the [SimpleResult] object.
+     *
+     * If validation fails, it will return a [SimpleResult] with the errors
+     */
+    override fun execute(): SimpleResult<Long, Multimap<ErrorTag, String>> {
+        val errors = validateRequest()
+
+        if(!errors.isEmpty)
+            return SimpleResult(null, errors)
+
+        val appointment = appointmentRepo.findById(appointmentId).get()
+        appointment.isPublic = true
+        appointmentRepo.save(appointment)
+
+        return SimpleResult(appointment.id, null)
+    }
+
+    /**
+     * Method responsible for constraint checking and validations for the appointment
+     * parameters. It will ensure the appointment exist and that the appointment is not
+     * already public.
+     */
+    private fun validateRequest(): Multimap<ErrorTag, String> {
+        var errors = HashMultimap.create<ErrorTag, String>()
+
+        if(!appointmentRepo.existsById(appointmentId))
+            errors.put(ErrorTag.ID, "Appointment does not exist with specified Id")
+        else if(appointmentRepo.findById(appointmentId).get().isPublic)
+            errors.put(ErrorTag.PUBLIC, "Appointment is already public")
+
+        return errors
+    }
+}
---
+++ src/test/kotlin/com/radiotelescope/contracts/appointment/MakePublicTest.kt
@@ -0,0 +1,124 @@
+package com.radiotelescope.contracts.appointment
+
+import com.radiotelescope.TestUtil
+import com.radiotelescope.repository.appointment.Appointment
+import com.radiotelescope.repository.appointment.IAppointmentRepository
+import com.radiotelescope.repository.telescope.ITelescopeRepository
+import com.radiotelescope.repository.user.User
+import liquibase.integration.spring.SpringLiquibase
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Bean
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.context.junit4.SpringRunner
+import java.util.*
+
+@DataJpaTest
+@RunWith(SpringRunner::class)
+@ActiveProfiles(value = ["test"])
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = ["classpath:sql/seedTelescope.sql"])
+internal class MakePublicTest {
+    class UtilTestContextConfiguration {
+        @Bean
+        fun utilService(): TestUtil { return TestUtil() }
+
+        @Bean
+        fun liquibase(): SpringLiquibase {
+            val liquibase = SpringLiquibase()
+            liquibase.setShouldRun(false)
+            return liquibase
+        }
+    }
+
+    @Autowired
+    private lateinit var testUtil: TestUtil
+
+    @Autowired
+    private lateinit var appointmentRepo: IAppointmentRepository
+
+    @Autowired
+    private lateinit var telescopeRepo: ITelescopeRepository
+
+
+    private lateinit var appointment: Appointment
+    private lateinit var appointmentAlreadyPublic: Appointment
+    private lateinit var user: User
+
+    private var appointmentId = -1L
+    private var appointmentAlreadyPublicId = -1L
+
+    @Before
+    fun setUp() {
+        // Make sure the sql script was executed
+        Assert.assertEquals(1, telescopeRepo.count())
+
+        // Persist the user
+        user = testUtil.createUser(
+                email = "rpim@ycp.edu"
+        )
+
+        // Persist the appointment
+        appointment = testUtil.createAppointment(
+                user = user,
+                startTime = Date(System.currentTimeMillis() + 10000L),
+                endTime = Date(System.currentTimeMillis() + 30000L),
+                isPublic = false,
+                status = Appointment.Status.Scheduled,
+                telescopeId = 1L
+        )
+        appointmentAlreadyPublic = testUtil.createAppointment(
+                user = user,
+                startTime = Date(System.currentTimeMillis() + 40000L),
+                endTime = Date(System.currentTimeMillis() + 50000L),
+                isPublic = true,
+                status = Appointment.Status.Scheduled,
+                telescopeId = 1L
+        )
+        appointmentId = appointment.id
+        appointmentAlreadyPublicId = appointmentAlreadyPublic.id
+    }
+
+    @Test
+    fun testValid_AppointmentExist_Success(){
+        val (id, errors) = MakePublic(
+                appointmentId = appointmentId,
+                appointmentRepo = appointmentRepo
+        ).execute()
+
+        // Make sure it was not error
+        Assert.assertNotNull(id)
+        Assert.assertNull(errors)
+    }
+
+    @Test
+    fun testInvalid_AppointmentDoesNotExist_Failure(){
+        val (id, errors) = MakePublic(
+                appointmentId = 1L,
+                appointmentRepo = appointmentRepo
+        ).execute()
+
+        // Make sure it was an error
+        Assert.assertNull(id)
+        Assert.assertNotNull(errors)
+        Assert.assertTrue(errors!![ErrorTag.ID].isNotEmpty())
+    }
+
+    @Test
+    fun testInvalid_AppointmentAlreadyPublic_Failure(){
+        val (id, errors) = MakePublic(
+                appointmentId = appointmentAlreadyPublicId,
+                appointmentRepo = appointmentRepo
+        ).execute()
+
+        // Make sure it was an error
+        Assert.assertNull(id)
+        Assert.assertNotNull(errors)
+        Assert.assertTrue(errors!![ErrorTag.PUBLIC].isNotEmpty())
+    }
+
+}
