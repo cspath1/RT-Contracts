dbf848bcec579b0d51913da99d33eec2b23fc461
albatronix <jamoros@ycp.edu>
2018-11-05 10:18:14 -0500

    Cleanup on HasOverlapCreate, HasOverlapUpdate and respective test classes

--- src/main/kotlin/com/radiotelescope/service/HasOverlapUpdate.kt
+++ src/main/kotlin/com/radiotelescope/service/HasOverlapUpdate.kt
@@ -6,61 +6,23 @@ import com.radiotelescope.repository.appointment.IAppointmentRepository
 import com.radiotelescope.repository.user.IUserRepository
 
 
-public class HasOverlapUpdate(val appointmentRepo:IAppointmentRepository
-                              )
+public class HasOverlapUpdate(val appointmentRepo:IAppointmentRepository)
 {
-
-        public fun hasOverlap(request: Update.Request):Boolean
-{
+    public fun hasOverlap(request: Update.Request):Boolean {
     var isOverlap = false
     val listAppts:List<Appointment> = appointmentRepo.selectAppointmentsWithinPotentialAppointmentTimeRange(request.endTime, request.startTime, request.telescopeId)
-    val one:Long = 1L
     if (!listAppts.isEmpty())
     { //if it's not empty,  there IS an overlap, cannot schedule
         isOverlap = true
-
-      //  println("size of listAppts: " + listAppts.size)
-
-
-
         for (a in listAppts)
         {
-
+            //if the ONLY appointment that conflicts with the requested appt is ITSELF, then no conflict
             if (listAppts.size == 1 && a.id == request.id)
             {
                 isOverlap = false
                 break
             }
-
-            /*
-            println("appt id of a:" + a.id)
-            println("lastIndex in listAppts: " + listAppts.lastIndex)
-            println("isOverlap is true; appt id of the user who scheduled the appointment already in the table: " + a.id)
-
-            //      println("isOverlap is true; user id of the user who requested a new appointment: " + request.id)
-
-            println("isOverlap is true; start time of the appointment already in the table : " + a.startTime)
-            println("isOverlap is true;  end time of the appointment already in the table : " + a.endTime)
-
-            println("isOverlap is true; start time of the appointment that was to be scheduled: " + request.startTime)
-            println("isOverlap is true; end time of the appointment that was to be scheduled: " + request.endTime)
-
-            println("startTime of appointment in index 0 of listAppts: " + listAppts.get(0).startTime)
-*/
-            //if the ONLY appointment that conflicts with the requested appt is ITSELF, then no conflict
-
-
-
         }
-
-
-
-    }
-    else if(listAppts.isEmpty())
-    {
-    //if it IS empty, no overlap, can schedule,
-        isOverlap = false
-
     }
 
     return isOverlap
--- src/test/kotlin/com/radiotelescope/service/HasOverlapUpdateTest.kt
+++ src/test/kotlin/com/radiotelescope/service/HasOverlapUpdateTest.kt
@@ -29,12 +29,11 @@ import java.util.*
 @RunWith(SpringRunner::class)
 @ActiveProfiles(value = ["test"])
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = ["classpath:sql/seedTelescope.sql"])
-class HasOverlapUpdateTest()
+class HasOverlapUpdateTest
 {
     @TestConfiguration
-    internal class UtilTestContextConfiguration()
+    internal class UtilTestContextConfiguration
     {
-
         @Bean
         fun utilService(): TestUtil {return TestUtil() }
 
@@ -61,7 +60,6 @@ class HasOverlapUpdateTest()
 
     @Autowired
     private lateinit var userRepo: IUserRepository
-
 
     var u1:User = User("Josh", "123", "j@ycp.edu", "hahaHilarious")
     var u2:User = User("SupFool", "abc", "quixotic@ycp.edu", "hahaFinn")
@@ -94,156 +92,81 @@ class HasOverlapUpdateTest()
     public fun updateToRightOverlap()
     {
         val hou = HasOverlapUpdate(appointmentRepo)
-
-
-       val dd: Date = Date(d.time + 5000)
+       val dd:Date = Date(d.time + 5000)
 
         //associated with a1
         val uR = Update.Request(a1_id, 1, Date(d.time + 90000), Date(dd.time + 110000), true)
         val uObject = Update(uR, appointmentRepo, telescopeRepo, userRoleRepo)
 
-        //In this case, this appointment request should now not be scheduled because of the check in Create
+        //In this case, this appointment request should now NOT be scheduled because of the check in Create
         val executed = uObject.execute()
         val s = executed.success
 
         //e should be populated
         val e: Multimap<ErrorTag, String>? = executed.error
-
-        if (s == null) //then e is populated
+        if (s == null)
         {
             if (!hou.hasOverlap(uR)) {
                 fail()
             }
-/*
-            for (ee in ErrorTag.values())
-            {
-                println("ErrorTag values: " + e?.get(ee))
 
-            }
-*/
             println("error should be the overlap error: " + e?.get(ErrorTag.OVERLAP).toString())
-            if ((e?.get(ErrorTag.OVERLAP).toString().equals("[Appointment cannot be rescheduled: Conflict with an already-scheduled appointment]"))) {
-
+            if ((e?.get(ErrorTag.OVERLAP).toString().equals("[Appointment cannot be rescheduled: Conflict with an already-scheduled appointment]")))
+            {
+            //pass
             }
             else fail()
-            /*
-            for (ee in ErrorTag.values())
-            {
-                println("is this populated? " + e?.get(ee))
-            }
 
-            println()
-            fail()
-
-            */
         } else {
             fail()
-
         }
-
-
-
     }
 
     @Test
     public fun updateToLeftOverlap() {
 
         val hou = HasOverlapUpdate(appointmentRepo)
-      //  println("d is: " + d)
-
         val dd: Date = Date(d.time + 5000)
-
-        //this one actually does get inserted into the database appointment table
         val uR = Update.Request(a1_id, 1, dd, Date(dd.time + 70000), true)
-
-        //At this point, two appointments are in the table. Starttime d to d+5, endtime d+6 to d+10-- no conflict
-        //  println("a1 aid: " + a1.id)
-        //   println("a2 aid: " + a2.id)
-        //uR is associated with a1
-
         val uObject = Update(uR, appointmentRepo, telescopeRepo, userRoleRepo)
 
-        //In this case, this appointment request should now not be scheduled because of the check in Create
+        //In this case, this appointment request should now NOT be scheduled because of the check in Create
         val executed = uObject.execute()
-
         val s = executed.success
-
         //e should be populated
         val e: Multimap<ErrorTag, String>? = executed.error
-
         if (s == null) //then e is populated
         {
-
             if (!hou.hasOverlap(uR)) {
                 fail()
             }
-/*
-            for (ee in ErrorTag.values())
-            {
-                println("ErrorTag values: " + e?.get(ee))
-
-            }
-*/
             println("error should be the overlap error: " + e?.get(ErrorTag.OVERLAP).toString())
-            if ((e?.get(ErrorTag.OVERLAP).toString().equals("[Appointment cannot be rescheduled: Conflict with an already-scheduled appointment]"))) {
-
+            if ((e?.get(ErrorTag.OVERLAP).toString().equals("[Appointment cannot be rescheduled: Conflict with an already-scheduled appointment]")))
+            { //pass
             }
             else fail()
-
-            /*
-            for (ee in ErrorTag.values())
-            {
-                println("is this populated? " + e?.get(ee))
-            }
-
-            println()
-            fail()
-
-            */
         } else {
             fail()
-
         }
-
-
-        //At this point, we still should have the original two appointments in the database table, with the original start
-        //and end Times
-
-
-
     }
 
     @Test
     public fun updateToFutureNoOverlap() {
 
         val uR = Update.Request(a1_id, 1, Date(d.time + 300000), Date(d.time + 350000), true)
-
         val hou = HasOverlapUpdate(appointmentRepo)
-
         if (!hou.hasOverlap(uR))
         {
-
         }
         else {
             fail()
         }
-
        val uE =  Update(uR, appointmentRepo, telescopeRepo, userRoleRepo).execute()
        val ss =  uE.success
        val ee = uE.error
-
-
-        if (ss == null)
-        {
-            for (eee in ErrorTag.values())
-            {
-               println("ErrorTag value:" + ee?.get(eee))
-
-            }
-
+       if (ss == null) {
             fail()
         }
-
         else //success is not null, so we do not fail
         {
         }
@@ -253,13 +176,11 @@ class HasOverlapUpdateTest()
     public fun updateToBeforeAnAppointmentNoOverlap()
     {
       val hou = HasOverlapUpdate(appointmentRepo)
-
-        //associated with a1
-        val uR = Update.Request(a1_id, 1, Date(Date().time + 3000), Date(d.time - 10000), true)
+      //associated with a1
+      val uR = Update.Request(a1_id, 1, Date(Date().time + 3000), Date(d.time - 10000), true)
 
         if (!hou.hasOverlap(uR))
         {
-
         }
         else {
             fail()
@@ -271,11 +192,6 @@ class HasOverlapUpdateTest()
 
         if (ss == null)
         {
-            for (eee in ErrorTag.values())
-            {
-                println("ErrorTag value:" + ee?.get(eee))
-
-            }
             fail()
         }
         else //success is not null, so we do not fail
@@ -285,38 +201,14 @@ class HasOverlapUpdateTest()
 
     @Test
     public fun rescheduleSameAppointmentOnTopOfOriginalRangeNoConflict() {
-
-
         val hou = HasOverlapUpdate(appointmentRepo)
-
-
-
         //normally would be a conflict, but because the only appointment in conflict is the actual one being updated, we're good
-        //Must change the query method for update
         val uR = Update.Request(a1_id, 1, d, Date(d.time + 55000), true)
-
-   //     println("d is: " + d)
-
-/*
-        if (!hou.hasOverlap(uR))
-        {
-
-        }
-        else
-        {
-            fail()
-        }
-        */
-
         val uE =  Update(uR, appointmentRepo, telescopeRepo, userRoleRepo).execute()
         val ss =  uE.success
         val ee = uE.error
         if (ss == null)
         {
-            for (eee in ErrorTag.values())
-            {
-                println("ErrorTag value:" + ee?.get(eee))
-            }
             fail()
         }
 
@@ -329,40 +221,22 @@ class HasOverlapUpdateTest()
     public fun requestedApptExtendsOverAnAppointment()
     {
 
-      val hou = HasOverlapUpdate(appointmentRepo)
-
+        val hou = HasOverlapUpdate(appointmentRepo)
         val uR = Update.Request(a1_id, 1, Date(d.time + 40000), Date(d.time + 70000), true)
         val uObject = Update(uR, appointmentRepo, telescopeRepo, userRoleRepo)
         val executed = uObject.execute()
         val s = executed.success
         //e should be populated
         val e: Multimap<ErrorTag, String>? = executed.error
-        if (s == null) //then e is populated
+        if (s == null)
         {
             if (!hou.hasOverlap(uR)) {
                 fail()
             }
-/*
-            for (ee in ErrorTag.values())
-            {
-                println("ErrorTag values: " + e?.get(ee))
-            }
-*/
             println("error should be the overlap error: " + e?.get(ErrorTag.OVERLAP).toString())
             if ((e?.get(ErrorTag.OVERLAP).toString().equals("[Appointment cannot be rescheduled: Conflict with an already-scheduled appointment]"))) {
             }
             else fail()
-
-            /*
-            for (ee in ErrorTag.values())
-            {
-                println("is this populated? " + e?.get(ee))
-            }
-
-            println()
-            fail()
-
-            */
         } else {
             fail()
         }
@@ -372,43 +246,23 @@ class HasOverlapUpdateTest()
     public fun requestedApptInsideAnAppt()
     {
         val hou = HasOverlapUpdate(appointmentRepo)
-
         val uR = Update.Request(a1_id, 1, Date(d.time + 70000), Date(d.time + 80000), true)
         val uObject = Update(uR, appointmentRepo, telescopeRepo, userRoleRepo)
         val executed = uObject.execute()
         val s = executed.success
         //e should be populated
         val e: Multimap<ErrorTag, String>? = executed.error
-        if (s == null) //then e is populated
+        if (s == null)
         {
             if (!hou.hasOverlap(uR)) {
                 fail()
             }
-/*
-            for (ee in ErrorTag.values())
-            {
-                println("ErrorTag values: " + e?.get(ee))
-            }
-*/
             println("error should be the overlap error: " + e?.get(ErrorTag.OVERLAP).toString())
             if ((e?.get(ErrorTag.OVERLAP).toString().equals("[Appointment cannot be rescheduled: Conflict with an already-scheduled appointment]"))) {
             }
             else fail()
-
-            /*
-            for (ee in ErrorTag.values())
-            {
-                println("is this populated? " + e?.get(ee))
-            }
-
-            println()
-            fail()
-
-            */
         } else {
             fail()
         }
-
-
     }
 }
