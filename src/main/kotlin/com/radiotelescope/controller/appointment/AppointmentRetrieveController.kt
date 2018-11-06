f705f8539d65e507394aa9f0566d23ebc69f13b8
cody <cspath1@ycp.edu>
2018-11-05 23:00:03 -0500

    Fix error where status codes were backwards

--- src/main/kotlin/com/radiotelescope/controller/appointment/AppointmentRetrieveController.kt
+++ src/main/kotlin/com/radiotelescope/controller/appointment/AppointmentRetrieveController.kt
@@ -80,12 +80,12 @@ class AppointmentRetrieveController(
 
             // Set the errors depending on if the user was not authenticated or the
             // record did not exists
-            result = if (it.missingRoles != null) {
-                Result(errors = it.toStringMap(), status = HttpStatus.NOT_FOUND)
+            result = if (it.missingRoles == null) {
+                Result(errors = it.invalidResourceId!!, status = HttpStatus.NOT_FOUND)
             }
             // user did not have access to the resource
             else {
-                Result(errors = it.invalidResourceId!!, status = HttpStatus.FORBIDDEN)
+                Result(errors = it.toStringMap(), status = HttpStatus.FORBIDDEN)
             }
         }
 
