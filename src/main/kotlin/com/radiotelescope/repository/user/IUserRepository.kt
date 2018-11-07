105bd9e4729f4b76fd3400979486a1bab026df8b
albatronix <jamoros@ycp.edu>
2018-11-07 16:06:00 -0500

    (no change)

--- src/main/kotlin/com/radiotelescope/repository/user/IUserRepository.kt
+++ src/main/kotlin/com/radiotelescope/repository/user/IUserRepository.kt
@@ -1,5 +1,8 @@
 package com.radiotelescope.repository.user
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
@@ -24,6 +27,9 @@ interface IUserRepository : PagingAndSortingRepository<User, Long> {
      * @return a [User]
      */
     fun findByEmail(email: String): User?
+
+
+
 
 
 }
