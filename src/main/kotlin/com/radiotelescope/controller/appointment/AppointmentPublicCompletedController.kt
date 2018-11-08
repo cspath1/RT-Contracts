b5bef80275a19996a7d4a69aff4efa2b458a5d6b
cody <cspath1@ycp.edu>
2018-11-07 21:49:44 -0500

    Implement AppointmentPublicCompletedController

---
+++ src/main/kotlin/com/radiotelescope/controller/appointment/AppointmentPublicCompletedController.kt
@@ -0,0 +1,111 @@
+package com.radiotelescope.controller.appointment
+
+import com.google.common.collect.HashMultimap
+import com.radiotelescope.contracts.appointment.UserAppointmentWrapper
+import com.radiotelescope.contracts.user.ErrorTag
+import com.radiotelescope.controller.BaseRestController
+import com.radiotelescope.controller.model.Result
+import com.radiotelescope.controller.spring.Logger
+import com.radiotelescope.repository.log.Log
+import com.radiotelescope.toStringMap
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Rest Controller to handle retrieving a list of public completed appointments
+ *
+ * @param appointmentWrapper the [UserAppointmentWrapper]
+ * @param logger the [Logger] service
+ */
+@RestController
+class AppointmentPublicCompletedController(
+        private val appointmentWrapper: UserAppointmentWrapper,
+        logger: Logger
+) : BaseRestController(logger) {
+    /**
+     * Execute method that is in charge of, given the page parameters were passed in
+     * correctly, calling the [UserAppointmentWrapper.publicCompletedAppointments]
+     * method and responding back to the client based on if the user was authenticated
+     * or not.
+     */
+    @GetMapping(value = ["/api/appointments/publicCompleted"])
+    fun execute(@RequestParam("page") pageNumber: Int?,
+                @RequestParam("size") pageSize: Int?): Result {
+        if ((pageNumber == null || pageNumber < 0) || (pageSize == null || pageSize <= 0)) {
+            val errors = pageErrors()
+            // Create error logs
+            logger.createErrorLogs(
+                    info = Logger.createInfo(
+                            affectedTable = Log.AffectedTable.APPOINTMENT,
+                            action = "Future Telescope Appointments LogList Retrieval",
+                            affectedRecordId = null
+                    ),
+                    errors = errors.toStringMap()
+            )
+
+            result = Result(errors = errors.toStringMap())
+        } else {
+            // Sort by most recent
+            val sort = Sort(Sort.Direction.DESC, "end_time")
+            appointmentWrapper.publicCompletedAppointments(
+                    pageable = PageRequest.of(pageNumber, pageSize, sort)
+            ) {
+                // If the command was a success
+                it.success?.let { page ->
+                    // Create success log
+                    logger.createSuccessLog(
+                            info = Logger.createInfo(
+                                    affectedTable = Log.AffectedTable.APPOINTMENT,
+                                    action = "Public Completed Appointments",
+                                    affectedRecordId = null
+                            )
+                    )
+
+                    result = Result(data = page)
+                }
+                // If the command was a failure
+                it.error?.let { errors ->
+                    logger.createErrorLogs(
+                            info = Logger.createInfo(
+                                    affectedTable = Log.AffectedTable.APPOINTMENT,
+                                    action = "Public Completed Appointments",
+                                    affectedRecordId = null
+                            ),
+                            errors = errors.toStringMap()
+                    )
+
+                    result = Result(errors = errors.toStringMap())
+                }
+            }?.let {
+                // If we get here, this means the User did not pass validation
+                // Create error logs
+                logger.createErrorLogs(
+                        info = Logger.createInfo(
+                                affectedTable = Log.AffectedTable.APPOINTMENT,
+                                action = "Future Telescope Appointments LogList Retrieval",
+                                affectedRecordId = null
+                        ),
+                        errors = it.toStringMap()
+                )
+
+                result = Result(errors = it.toStringMap(), status = HttpStatus.FORBIDDEN)
+            }
+        }
+
+        return result
+    }
+
+    /**
+     * Private method to return a [HashMultimap] of errors in the
+     * event that invalid page parameters were provided
+     */
+    private fun pageErrors(): HashMultimap<ErrorTag, String> {
+        val errors = HashMultimap.create<ErrorTag, String>()
+        errors.put(ErrorTag.PAGE_PARAMS, "Invalid page parameters")
+        return errors
+    }
+}
