1e1b2d7a98dade7882820c9aeb084d5f96eb076e
albatronix <jamoros@ycp.edu>
2018-11-09 22:50:34 -0500

    Cleanup HasOverlap

--- src/main/kotlin/com/radiotelescope/service/HasOverlap.kt
+++ src/main/kotlin/com/radiotelescope/service/HasOverlap.kt
@@ -20,45 +20,18 @@ class HasOverlap(val appointmentRepo:IAppointmentRepository)
      * @return a [Boolean], true if conflict, false if no conflict
      */
 
-    public fun hasOverlapUpdate(request: Update.Request):List<Appointment> {
-    var isOverlap = false
+    fun hasOverlapUpdate(request: Update.Request):List<Appointment> {
     val listAppts:List<Appointment> = appointmentRepo.selectAppointmentsWithinPotentialAppointmentTimeRange(request.endTime, request.startTime, request.telescopeId)
-
         return listAppts
-       /*
-    if (!listAppts.isEmpty())
-    { //if it's not empty,  there IS an overlap, cannot schedule
-        isOverlap = true
-        for (a in listAppts)
-        {
-            //if the ONLY appointment that conflicts with the requested appt is ITSELF, then no conflict
-            if (listAppts.size == 1 && a.id == request.id)
-            {
-                isOverlap = false
-                break
-            }
-        }
-    }
-    */
-
-    //return isOverlap
 }
-
-    public fun hasOverlapCreate(
-            request: Create.Request
-    ): List<Appointment> {
-
-       // var isOverlap = false
+    /**
+     * Method checks if there are any conflicts when re-scheduling an appointment.
+     *
+     * @param the [Create.Request], containing the details of the potential appointment
+     * @return a [Boolean], true if conflict, false if no conflict
+     */
+   fun hasOverlapCreate(request: Create.Request): List<Appointment> {
         val listAppts: List<Appointment> = appointmentRepo.selectAppointmentsWithinPotentialAppointmentTimeRange(request.endTime, request.startTime, request.telescopeId)
-      /*
-        //if at least one appt returned, there IS an overlap
-        if (!listAppts.isEmpty()) {
-            isOverlap = true
-        }
-        return isOverlap
-        */
         return listAppts
     }
-
-
 }
