4b43cfdf20d321ff4d6e60db6514d00640cc2a05
cody <cspath1@ycp.edu>
2018-11-10 00:29:22 -0500

    Modify new database migration to be compatible with existing records

--- src/main/resources/db/changelog/11-add-user-account-hash.sql
+++ src/main/resources/db/changelog/11-add-user-account-hash.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
 --changeset cspath1:13
-ALTER TABLE user ADD account_hash VARCHAR(150) NOT NULL;
-CREATE UNIQUE INDEX user_account_hash_uindex ON user (account_hash);
+ALTER TABLE user ADD account_hash VARCHAR(150) DEFAULT NULL;
+CREATE UNIQUE INDEX account_hash ON user (account_hash);
 -- ALTER TABLE user DROP account_hash;
