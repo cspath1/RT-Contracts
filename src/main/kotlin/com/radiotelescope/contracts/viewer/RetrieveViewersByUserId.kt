f9a7d9d190f1a896ab41faef3ac005f318fe0188
albatronix <jamoros@ycp.edu>
2018-11-12 15:46:47 -0500

    Change to give RetrieveViewersByUserId a Viewer.Request rather than a sharing user id and a viewer id

--- src/main/kotlin/com/radiotelescope/contracts/viewer/RetrieveViewersByUserId.kt
+++ src/main/kotlin/com/radiotelescope/contracts/viewer/RetrieveViewersByUserId.kt
@@ -15,32 +15,36 @@ import org.springframework.data.domain.Pageable
 class RetrieveViewersByUserId(
         private val viewerRepo: IViewerRepository,
         private val userRepo: IUserRepository,
-        private val sharing_user_id:Long,
-        private val viewer_id:Long,
+     //   private val sharing_user_id:Long,
+      //  private val viewer_id:Long,
+        private val request: Create.Request,
         private val pageable: Pageable
         ):Command<MutableList<Appointment>, Multimap<ErrorTag, String>> {
 
-override fun execute(): SimpleResult<MutableList<Appointment>, Multimap<ErrorTag, String>> {
+    override fun execute(): SimpleResult<MutableList<Appointment>, Multimap<ErrorTag, String>> {
 
-val errors = HashMultimap.create<ErrorTag, String>()
+        with(request)
+        {
+            val errors = HashMultimap.create<ErrorTag, String>()
 
-if (!userRepo.existsById(sharing_user_id))
-{
-    errors.put(ErrorTag.USER_ID, "user id ${sharing_user_id} does not exist")
-    return SimpleResult(null, errors)
-}
-if (!userRepo.existsById(viewer_id))
-{
-    errors.put(ErrorTag.VIEWER_ID, "viewer with user id ${viewer_id} does not exist")
-    return SimpleResult(null, errors)
-}
-    val viewers: Page<Viewer> = viewerRepo.getViewersOfAppointmentBySharingUserId(sharing_user_id, pageable)
-    val sharedAppointments: MutableList<Appointment>? = arrayListOf()
+            if (!userRepo.existsById(sharinguserId)) {
+                errors.put(ErrorTag.USER_ID, "user id ${sharinguserId} does not exist")
+                return SimpleResult(null, errors)
+            }
+            if (!userRepo.existsById(viewerId)) {
+                errors.put(ErrorTag.VIEWER_ID, "viewer with user id ${viewerId} does not exist")
+                return SimpleResult(null, errors)
+            }
+            val viewers: Page<Viewer> = viewerRepo.getViewersOfAppointmentBySharingUserId(sharinguserId, pageable)
+            val sharedAppointments: MutableList<Appointment>? = arrayListOf()
 
-    for (v in viewers)
-    {
-        sharedAppointments?.add(v.appointment!!)
-    }
-       return SimpleResult(sharedAppointments,null)
+            for (v in viewers) {
+                sharedAppointments?.add(v.appointment!!)
+            }
+            return SimpleResult(sharedAppointments, null)
+
+
+        }
+
     }
 }
