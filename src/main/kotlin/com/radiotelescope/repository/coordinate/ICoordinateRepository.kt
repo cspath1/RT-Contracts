df0917713a21cec18332eef6aa84dc1e06b04c72
cody <cspath1@ycp.edu>
2018-11-13 22:25:39 -0500

    Add documentation for Coordinate Entity and Repo

--- src/main/kotlin/com/radiotelescope/repository/coordinate/ICoordinateRepository.kt
+++ src/main/kotlin/com/radiotelescope/repository/coordinate/ICoordinateRepository.kt
@@ -2,4 +2,7 @@ package com.radiotelescope.repository.coordinate
 
 import org.springframework.data.repository.CrudRepository
 
+/**
+ * Spring Repository for the [Coordinate] Entity
+ */
 interface ICoordinateRepository : CrudRepository<Coordinate, Long>
