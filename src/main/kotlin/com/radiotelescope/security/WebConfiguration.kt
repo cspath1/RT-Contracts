fa0fcba80b01eed061a34672f8534633725e68ac
Derek McClellan <dmcclellan@ycp.edu>
2018-12-01 17:54:36 -0500

    Attempt to enable CORS

--- src/main/kotlin/com/radiotelescope/security/WebConfiguration.kt
+++ src/main/kotlin/com/radiotelescope/security/WebConfiguration.kt
@@ -3,18 +3,22 @@ package com.radiotelescope.security
 import com.radiotelescope.controller.model.Profile
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableWebMvc
 class WebConfiguration(
         val profile: Profile
 ) : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         if (profile == Profile.PROD) {
-            registry.addMapping("/**")
-                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-                    .allowedOrigins("*")
+            registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "DELETE", "OPTIONS", "PUT")
+                    .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                            "Access-Control-Request-Headers")
+                    .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                    .allowCredentials(true).maxAge(3600);
         }
     }
 }
