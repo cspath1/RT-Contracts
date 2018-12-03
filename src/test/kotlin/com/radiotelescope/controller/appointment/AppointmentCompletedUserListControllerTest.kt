89da04e9341fed200b0b81cba9fdc2119c8a01f1
cody <cspath1@ycp.edu>
2018-12-03 01:36:37 -0500

    Implement test for AppointmentCompletedUserListController

---
+++ src/test/kotlin/com/radiotelescope/controller/appointment/AppointmentCompletedUserListControllerTest.kt
@@ -0,0 +1,160 @@
+package com.radiotelescope.controller.appointment
+
+import com.radiotelescope.TestUtil
+import com.radiotelescope.repository.appointment.Appointment
+import com.radiotelescope.repository.log.ILogRepository
+import com.radiotelescope.repository.role.UserRole
+import com.radiotelescope.repository.user.User
+import liquibase.integration.spring.SpringLiquibase
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.data.domain.Page
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+import java.util.*
+
+@DataJpaTest
+@RunWith(SpringRunner::class)
+@ActiveProfiles(value = ["test"])
+internal class AppointmentCompletedUserListControllerTest : BaseAppointmentRestControllerTest() {
+    @TestConfiguration
+    class UtilTestContextConfiguration {
+        @Bean
+        fun utilService(): TestUtil { return TestUtil() }
+
+        @Bean
+        fun liquibase(): SpringLiquibase {
+            val liquibase = SpringLiquibase()
+            liquibase.setShouldRun(false)
+            return liquibase
+        }
+    }
+
+    @Autowired
+    private lateinit var testUtil: TestUtil
+
+    @Autowired
+    private lateinit var logRepo: ILogRepository
+
+    private lateinit var appointmentCompletedUserListController: AppointmentCompletedUserListController
+    private lateinit var user: User
+
+    @Before
+    override fun init() {
+        super.init()
+
+        appointmentCompletedUserListController = AppointmentCompletedUserListController(
+                appointmentWrapper = getWrapper(),
+                logger = getLogger()
+        )
+
+        user = testUtil.createUser("cspath1@ycp.edu")
+
+        // Create two appointments for the user
+        testUtil.createAppointment(
+                user = user,
+                telescopeId = 1L,
+                status = Appointment.Status.COMPLETED,
+                startTime = Date(System.currentTimeMillis() - 10000L),
+                endTime = Date(System.currentTimeMillis() - 5000L),
+                isPublic = true
+        )
+
+        testUtil.createAppointment(
+                user = user,
+                telescopeId = 1L,
+                status = Appointment.Status.COMPLETED,
+                startTime = Date(System.currentTimeMillis() - 20000L),
+                endTime = Date(System.currentTimeMillis() - 15000L),
+                isPublic = true
+        )
+    }
+
+    @Test
+    fun testSuccessResponse() {
+        // Simulate a login
+        getContext().login(user.id)
+        getContext().currentRoles.add(UserRole.Role.USER)
+
+        val result = appointmentCompletedUserListController.execute(
+                userId = user.id,
+                pageNumber = 0,
+                pageSize = 25
+        )
+
+        assertNotNull(result)
+        assertTrue(result.data is Page<*>)
+        assertEquals(HttpStatus.OK, result.status)
+        assertNull(result.errors)
+
+        // A log should have been created for each returned record (2)
+        assertEquals(2, logRepo.count())
+    }
+
+    @Test
+    fun testFailedValidationResponse() {
+        // Simulate a login as an admin
+        getContext().login(user.id)
+        getContext().currentRoles.addAll(listOf(UserRole.Role.USER, UserRole.Role.ADMIN))
+
+        val result = appointmentCompletedUserListController.execute(
+                311L,
+                pageNumber = 0,
+                pageSize = 25
+        )
+
+        assertNotNull(result)
+        assertNull(result.data)
+        assertEquals(HttpStatus.BAD_REQUEST, result.status)
+        assertNotNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+
+    @Test
+    fun testFailedAuthenticationResponse() {
+        // Do not log the user in
+        val result = appointmentCompletedUserListController.execute(
+                userId = user.id,
+                pageNumber = 0,
+                pageSize = 25
+        )
+
+        assertNotNull(result)
+        assertNull(result.data)
+        assertEquals(HttpStatus.FORBIDDEN, result.status)
+        assertNotNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+
+    @Test
+    fun testInvalidPageParametersResponse() {
+        // Simulate a login as an admin
+        getContext().login(user.id)
+        getContext().currentRoles.addAll(listOf(UserRole.Role.USER, UserRole.Role.ADMIN))
+
+        val result = appointmentCompletedUserListController.execute(
+                userId = user.id,
+                pageNumber = -311,
+                pageSize = -420
+        )
+
+        assertNotNull(result)
+        assertNull(result.data)
+        assertEquals(HttpStatus.BAD_REQUEST, result.status)
+        assertNotNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+}
