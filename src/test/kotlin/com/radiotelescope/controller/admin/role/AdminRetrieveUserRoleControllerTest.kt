83104f010d2976f808b2c7fd14f811347d592f56
rpim <rpim@ycp.edu>
2018-12-06 20:08:33 -0500

    Add AdminRetrieveUserRoleControllerTest

--- src/test/kotlin/com/radiotelescope/controller/admin/role/AdminRetrieveUserRoleControllerTest.kt
+++ src/test/kotlin/com/radiotelescope/controller/admin/role/AdminRetrieveUserRoleControllerTest.kt
@@ -1,18 +1,21 @@
-/*package com.radiotelescope.controller.admin.role
+package com.radiotelescope.controller.admin.role
 
 import com.radiotelescope.TestUtil
+import com.radiotelescope.contracts.role.UserRoleInfo
 import com.radiotelescope.controller.user.BaseUserRoleControllerTest
-import com.radiotelescope.repository.appointment.IAppointmentRepository
 import com.radiotelescope.repository.log.ILogRepository
 import com.radiotelescope.repository.role.UserRole
 import com.radiotelescope.repository.user.User
 import liquibase.integration.spring.SpringLiquibase
+import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 
@@ -39,10 +42,7 @@ internal class AdminRetrieveUserRoleControllerTest : BaseUserRoleControllerTest(
     @Autowired
     private lateinit var logRepo: ILogRepository
 
-    @Autowired
-    private lateinit var appointmentRepo: IAppointmentRepository
-
-    private lateinit var adminUserRoleValidateController: AdminUserRoleValidateController
+    private lateinit var adminRetrieveUserRoleController: AdminRetrieveUserRoleController
     private lateinit var admin: User
     private lateinit var roles: List<UserRole>
 
@@ -50,7 +50,7 @@ internal class AdminRetrieveUserRoleControllerTest : BaseUserRoleControllerTest(
     override fun init() {
         super.init()
 
-        adminUserRoleValidateController = AdminUserRoleValidateController(
+        adminRetrieveUserRoleController = AdminRetrieveUserRoleController(
                 roleWrapper = getWrapper(),
                 logger = getLogger()
         )
@@ -69,4 +69,60 @@ internal class AdminRetrieveUserRoleControllerTest : BaseUserRoleControllerTest(
                 isApproved = true
         )
     }
-}*/
+
+    @Test
+    fun testSuccessResponse() {
+        // Test the success scenario to ensure
+        // the result object is correctly set
+
+        // Simulate a login
+        getContext().login(admin.id)
+        getContext().currentRoles.addAll(listOf(UserRole.Role.ADMIN, UserRole.Role.USER))
+
+        val result = adminRetrieveUserRoleController.execute(
+                roleId = roles[1].id
+        )
+
+        assertNotNull(result)
+        assertTrue(result.data is UserRoleInfo)
+        assertEquals(HttpStatus.OK, result.status)
+        assertNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+
+    @Test
+    fun testFailedValidationResponse() {
+        // Simulate a login
+        getContext().login(admin.id)
+        getContext().currentRoles.addAll(listOf(UserRole.Role.ADMIN, UserRole.Role.USER))
+
+        val result = adminRetrieveUserRoleController.execute(
+                roleId = 123456789
+        )
+
+        assertNotNull(result)
+        assertNull(result.data)
+        assertEquals(HttpStatus.BAD_REQUEST, result.status)
+        assertNotNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+
+    @Test
+    fun testFailedAuthenticationResponse() {
+        // Do not log the user in
+        val result = adminRetrieveUserRoleController.execute(
+                roleId = roles[1].id
+        )
+        assertNotNull(result)
+        assertNull(result.data)
+        assertEquals(HttpStatus.FORBIDDEN, result.status)
+        assertNotNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+}
