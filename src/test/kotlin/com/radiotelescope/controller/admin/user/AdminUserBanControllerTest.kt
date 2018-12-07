8f8f25d4ecd0a2c410d1bdf6698ec1417616a895
rpim <rpim@ycp.edu>
2018-12-07 12:23:16 -0500

    Fix AdminUserBanController Add AdminUserBanControllerTest

--- src/test/kotlin/com/radiotelescope/controller/admin/user/AdminUserBanControllerTest.kt
+++ src/test/kotlin/com/radiotelescope/controller/admin/user/AdminUserBanControllerTest.kt
@@ -1,10 +1,12 @@
-package com.radiotelescope.controller.admin.role
+package com.radiotelescope.controller.admin.user
 
 import com.radiotelescope.TestUtil
-import com.radiotelescope.controller.user.BaseUserRoleControllerTest
+import com.radiotelescope.controller.user.BaseUserRestControllerTest
 import com.radiotelescope.repository.log.ILogRepository
 import com.radiotelescope.repository.role.UserRole
+import com.radiotelescope.repository.user.IUserRepository
 import com.radiotelescope.repository.user.User
+import com.radiotelescope.services.ses.MockAwsSesSendService
 import liquibase.integration.spring.SpringLiquibase
 import org.junit.Assert.*
 import org.junit.Before
@@ -14,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
@@ -22,7 +23,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @DataJpaTest
 @RunWith(SpringRunner::class)
 @ActiveProfiles(value = ["test"])
-internal class AdminUnapprovedUserRoleListControllerTest : BaseUserRoleControllerTest() {
+internal class AdminUserBanControllerTest : BaseUserRestControllerTest() {
     @TestConfiguration
     class UtilTestContextConfiguration {
         @Bean
@@ -42,31 +43,33 @@ internal class AdminUnapprovedUserRoleListControllerTest : BaseUserRoleControlle
     @Autowired
     private lateinit var logRepo: ILogRepository
 
-    private lateinit var adminUnapprovedUserRoleListController: AdminUnapprovedUserRoleListController
+    @Autowired
+    private lateinit var userRepo: IUserRepository
+
+    private lateinit var adminUserBanController: AdminUserBanController
     private lateinit var admin: User
+    private lateinit var user: User
 
     @Before
     override fun init() {
         super.init()
 
-        adminUnapprovedUserRoleListController = AdminUnapprovedUserRoleListController(
-                roleWrapper = getWrapper(),
-                logger = getLogger()
+        adminUserBanController = AdminUserBanController(
+                userRepo = userRepo,
+                userWrapper = getWrapper(),
+                logger = getLogger(),
+                awsSesSendService = MockAwsSesSendService(true)
         )
 
-        admin = testUtil.createUser("rpim@ycp.edu")
+        admin = testUtil.createUser("rpim1@ycp.edu")
         testUtil.createUserRolesForUser(
                 userId = admin.id,
                 role = UserRole.Role.ADMIN,
                 isApproved = true
         )
 
-        val user = testUtil.createUser("rpim2@ycp.edu")
-        testUtil.createUserRolesForUser(
-                userId = user.id,
-                role = UserRole.Role.GUEST,
-                isApproved = false
-        )
+        user = testUtil.createUser("rpim@ycp.edu")
+
     }
 
     @Test
@@ -78,34 +81,15 @@ internal class AdminUnapprovedUserRoleListControllerTest : BaseUserRoleControlle
         getContext().login(admin.id)
         getContext().currentRoles.addAll(listOf(UserRole.Role.ADMIN, UserRole.Role.USER))
 
-        val result = adminUnapprovedUserRoleListController.execute(
-                pageNumber = 0,
-                pageSize = 10
+        val result = adminUserBanController.execute(
+                message = "You have missed use our site",
+                userId = user.id
         )
 
         assertNotNull(result)
-        assertTrue(result.data is Page<*>)
+        assertTrue(result.data is Long)
         assertEquals(HttpStatus.OK, result.status)
         assertNull(result.errors)
-
-        // Ensure a log record was created
-        assertEquals(1, logRepo.count())
-    }
-
-    @Test
-    fun testFailedRequiredFieldResponse() {
-        // Simulate a login
-        getContext().login(admin.id)
-        getContext().currentRoles.addAll(listOf(UserRole.Role.ADMIN, UserRole.Role.USER))
-
-        val result = adminUnapprovedUserRoleListController.execute(
-                pageNumber = null,
-                pageSize = null
-        )
-        assertNotNull(result)
-        assertNull(result.data)
-        assertEquals(HttpStatus.BAD_REQUEST, result.status)
-        assertNotNull(result.errors)
 
         // Ensure a log record was created
         assertEquals(1, logRepo.count())
@@ -117,9 +101,9 @@ internal class AdminUnapprovedUserRoleListControllerTest : BaseUserRoleControlle
         getContext().login(admin.id)
         getContext().currentRoles.addAll(listOf(UserRole.Role.ADMIN, UserRole.Role.USER))
 
-        val result = adminUnapprovedUserRoleListController.execute(
-                pageNumber = -1,
-                pageSize = 0
+        val result = adminUserBanController.execute(
+                message = "You have missed use our site",
+                userId = 123456789
         )
         assertNotNull(result)
         assertNull(result.data)
@@ -133,9 +117,9 @@ internal class AdminUnapprovedUserRoleListControllerTest : BaseUserRoleControlle
     @Test
     fun testFailedAuthenticationResponse() {
         // Do not log the user in
-        val result = adminUnapprovedUserRoleListController.execute(
-                pageNumber = 0,
-                pageSize = 10
+        val result = adminUserBanController.execute(
+                message = "You have missed use our site",
+                userId = user.id
         )
         assertNotNull(result)
         assertNull(result.data)
@@ -145,4 +129,5 @@ internal class AdminUnapprovedUserRoleListControllerTest : BaseUserRoleControlle
         // Ensure a log record was created
         assertEquals(1, logRepo.count())
     }
+
 }
