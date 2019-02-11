d1b8ef91e7da0a6ceeaba2eda351e496e801e376
jmajeski <jacobmajeski@gmail.com>
2019-02-11 15:44:32 -0500

    Add UserNotificationType ErrorTag

---
+++ src/main/kotlin/com/radiotelescope/contracts/userNotificationType/ErrorTag.kt
@@ -0,0 +1,9 @@
+package com.radiotelescope.contracts.userNotificationType
+
+/**
+ * Enum representing field validation failures for the UserNotificationType Entity
+ */
+enum class ErrorTag {
+    ID,
+    TYPE
+}
