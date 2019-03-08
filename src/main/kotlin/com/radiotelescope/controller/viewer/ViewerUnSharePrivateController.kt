a47077c5f746442c4fa3bab96b5fb3afe6701db2
rpim <rpim@ycp.edu>
2019-03-07 23:13:34 -0500

    Implement ViewerUnSharePrivateController - minor fixes

--- src/main/kotlin/com/radiotelescope/controller/viewer/ViewerUnSharePrivateController.kt
+++ src/main/kotlin/com/radiotelescope/controller/viewer/ViewerUnSharePrivateController.kt
@@ -1,68 +1,47 @@
 package com.radiotelescope.controller.viewer
 
-import com.radiotelescope.contracts.viewer.SharePrivateAppointment
+import com.radiotelescope.contracts.viewer.UnSharePrivateAppointment
 import com.radiotelescope.contracts.viewer.UserViewerWrapper
 import com.radiotelescope.controller.BaseRestController
 import com.radiotelescope.controller.model.Result
-import com.radiotelescope.controller.model.ses.SendForm
 import com.radiotelescope.controller.spring.Logger
-import com.radiotelescope.repository.appointment.IAppointmentRepository
 import com.radiotelescope.repository.log.Log
-import com.radiotelescope.service.ses.IAwsSesSendService
 import com.radiotelescope.toStringMap
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
-/**
- * REST Controller to handle Sharing Private Appointment
- *
- * @param viewerWrapper the [UserViewerWrapper]
- * @param awsSesSendService the [IAwsSesSendService]
- * @param appointmentRepo the [IAppointmentRepository]
- * @param logger the [Logger] service
- */
 @RestController
-class ViewerSharePrivateController(
+class ViewerUnSharePrivateController(
         private val viewerWrapper: UserViewerWrapper,
-        private val awsSesSendService: IAwsSesSendService,
-        private val appointmentRepo: IAppointmentRepository,
         logger: Logger
 ) : BaseRestController(logger) {
-    /**
-     * Execute method that is in charge of sharing a private appointment by executing
-     * [UserViewerWrapper.sharePrivateAppointment] method
-     *
-     * If it was successful, it will notify the user that an appointment has been shared
-     * with them
-     */
+
     @CrossOrigin(value = ["http://localhost:8081"])
     @PostMapping(value = ["/api/appointments/{appointmentId}/viewers"])
     fun execute(@PathVariable("appointmentId") appointmentId: Long,
-                @RequestParam("email") email: String): Result {
-        val request = SharePrivateAppointment.Request(
-                email = email,
+                @RequestParam("userId") userId: Long): Result {
+        val request = UnSharePrivateAppointment.Request(
+                userId = userId,
                 appointmentId = appointmentId
         )
-        viewerWrapper.sharePrivateAppointment(request) {
+        viewerWrapper.unSharePrivateAppointment(request) {
             // If the command was a success
-            it.success?.let { id ->
+            it.success?.let {id ->
                 logger.createSuccessLog(
                         info = Logger.createInfo(Log.AffectedTable.VIEWER,
-                                action = "Share Private Appointment",
+                                action = "UnShare Private Appointment",
                                 affectedRecordId = id,
                                 status = HttpStatus.OK.value()
                         )
                 )
                 result = Result(data = id)
-
-                sendEmail(email, appointmentId)
             }
             // If the command was a failure
             it.error?.let { errors ->
                 logger.createErrorLogs(
                         info = Logger.createInfo(
                                 affectedTable = Log.AffectedTable.VIEWER,
-                                action = "Share Private Appointment",
+                                action = "UnShare Private Appointment",
                                 affectedRecordId = null,
                                 status = HttpStatus.BAD_REQUEST.value()
                         ),
@@ -78,13 +57,12 @@ class ViewerSharePrivateController(
             logger.createErrorLogs(
                     info = Logger.createInfo(
                             affectedTable = Log.AffectedTable.VIEWER,
-                            action = "Share Private Appointment",
+                            action = "UnShare Private Appointment",
                             affectedRecordId = null,
                             status = HttpStatus.FORBIDDEN.value()
                     ),
                     errors = if (it.missingRoles != null) it.toStringMap() else it.invalidResourceId!!
             )
-
             // Set the errors depending on if the user was not authenticated or the
             // record did not exists
             result = if (it.missingRoles == null) {
@@ -95,19 +73,6 @@ class ViewerSharePrivateController(
                 Result(errors = it.toStringMap(), status = HttpStatus.FORBIDDEN)
             }
         }
-
         return result
-    }
-
-
-    private fun sendEmail(email: String, appointmentId: Long) {
-        val sendForm = SendForm(
-                toAddresses = listOf(email),
-                fromAddress = "YCP Radio Telescope <cspath1@ycp.edu>",
-                subject = "Share Private Appointment",
-                htmlBody = "<p>Appointment #$appointmentId has been shared with you."
-        )
-
-        awsSesSendService.execute(sendForm)
     }
 }
---
+++ src/test/kotlin/com/radiotelescope/controller/viewer/ViewerUnSharePrivateAppointmentTest.kt
@@ -0,0 +1,161 @@
+package com.radiotelescope.controller.viewer
+
+import com.radiotelescope.TestUtil
+import com.radiotelescope.repository.appointment.Appointment
+import com.radiotelescope.repository.appointment.IAppointmentRepository
+import com.radiotelescope.repository.log.ILogRepository
+import com.radiotelescope.repository.role.UserRole
+import com.radiotelescope.repository.user.User
+import liquibase.integration.spring.SpringLiquibase
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+import java.util.*
+
+@DataJpaTest
+@RunWith(SpringRunner::class)
+@ActiveProfiles(value = ["test"])
+internal class ViewerUnSharePrivateAppointmentTest : BaseViewerRestControllerTest() {
+    @TestConfiguration
+    class UtilTestContextConfiguration {
+        @Bean
+        fun utilService(): TestUtil { return TestUtil() }
+
+        @Bean
+        fun liquibase(): SpringLiquibase {
+            val liquibase = SpringLiquibase()
+            liquibase.setShouldRun(false)
+            return liquibase
+        }
+    }
+
+    @Autowired
+    private lateinit var testUtil: TestUtil
+
+    @Autowired
+    private lateinit var logRepo: ILogRepository
+
+    @Autowired
+    private lateinit var appointmentRepo: IAppointmentRepository
+
+    private lateinit var viewerUnSharePrivateController: ViewerUnSharePrivateController
+    private lateinit var user: User
+    private lateinit var researcher: User
+    private lateinit var appointment: Appointment
+
+    @Before
+    override fun init() {
+        super.init()
+
+        viewerUnSharePrivateController = ViewerUnSharePrivateController(
+                viewerWrapper = getWrapper(),
+                logger = getLogger()
+        )
+        user = testUtil.createUser("rpim@ycp.edu")
+        researcher = testUtil.createUser("rpim1@ycp.edu")
+
+        appointment = testUtil.createAppointment(
+                user = researcher,
+                startTime = Date(System.currentTimeMillis() + 100000L),
+                endTime = Date(System.currentTimeMillis()  +  200000L),
+                status = Appointment.Status.SCHEDULED,
+                isPublic = false,
+                telescopeId = 1L
+        )
+
+        testUtil.createViewer(user, appointment)
+    }
+
+    @Test
+    fun testSuccessResponse() {
+        // Test the success scenario to ensure
+        // the result object is correctly set
+
+        // Simulate a login
+        getContext().login(researcher.id)
+        getContext().currentRoles.addAll(listOf(UserRole.Role.RESEARCHER, UserRole.Role.USER))
+
+        val result = viewerUnSharePrivateController.execute(
+                appointmentId = appointment.id,
+                userId = user.id
+        )
+
+        assertNotNull(result)
+        assertTrue(result.data is Long)
+        assertEquals(HttpStatus.OK, result.status)
+        assertNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+
+    @Test
+    fun testFailedValidationResponse() {
+        // Test the failure scenario to ensure
+        // the result object is correctly set
+
+        // Simulate a login
+        getContext().login(researcher.id)
+        getContext().currentRoles.addAll(listOf(UserRole.Role.RESEARCHER, UserRole.Role.USER))
+
+        val result = viewerUnSharePrivateController.execute(
+                appointmentId = appointment.id,
+                userId = 123456L
+        )
+
+        assertNotNull(result)
+        assertNull(result.data)
+        assertEquals(HttpStatus.BAD_REQUEST, result.status)
+        assertNotNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+
+    @Test
+    fun testFailedAuthenticationResponse() {
+        // Test the failure scenario to ensure
+        // the result object is correctly set
+
+        // Do not simulate a login
+
+        val result = viewerUnSharePrivateController.execute(
+                appointmentId = appointment.id,
+                userId = user.id
+        )
+
+        assertNotNull(result)
+        assertNull(result.data)
+        assertEquals(HttpStatus.FORBIDDEN, result.status)
+        assertNotNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+
+    @Test
+    fun testInvalidResourceIdResponse() {
+        // Test the failure scenario where the
+        // appointment id does not exist
+        val result = viewerUnSharePrivateController.execute(
+                appointmentId = 311L,
+                userId = user.id
+        )
+
+        assertNotNull(result)
+        assertNull(result.data)
+        assertEquals(HttpStatus.NOT_FOUND, result.status)
+        assertNotNull(result.errors)
+
+        // Ensure a log record was created
+        assertEquals(1, logRepo.count())
+    }
+}
