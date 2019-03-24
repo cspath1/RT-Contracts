8950e966b550fce55dbd7e6a9865309f0ad4ebfb
Cody Spath <cspath1@ycp.edu>
2019-03-24 18:14:13 -0400

    Add database migration for allotted_time_cap table

---
+++ src/main/resources/db/changelog/16-create-user-specific-time-caps.sql
@@ -0,0 +1,16 @@
+-- liquibase formatted sql
+
+-- changeset cspath1:19
+CREATE TABLE allotted_time_cap (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  user_id INT(11) NOT NULL,
+  allotted_time INT DEFAULT 0,
+
+  PRIMARY KEY (id)
+);
+-- rollback drop table allotted_time_cap
+
+-- changeset cspath1:20
+INSERT INTO allotted_time_cap (user_id, allotted_time)
+SELECT id, 5 FROM user;
+-- rollback delete from allotted_time_cap
