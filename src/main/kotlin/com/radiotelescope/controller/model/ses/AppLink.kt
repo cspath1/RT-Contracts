40eba1f2d3ffbc826d5e796732db606571bf9771
Cody Spath <cspath1@ycp.edu>
2019-04-09 21:12:26 -0400

    Fix AppLink

--- src/main/kotlin/com/radiotelescope/controller/model/ses/AppLink.kt
+++ src/main/kotlin/com/radiotelescope/controller/model/ses/AppLink.kt
@@ -6,7 +6,7 @@ abstract class AppLink {
     companion object {
         fun generate(profile: Profile): String {
             return if (profile == Profile.LOCAL || profile == Profile.DEV)
-                "http://localhost:8081"
+                "http://localhost:8081/#"
             else
                 "https://www.ycpradiotelescope.com/#"
         }
