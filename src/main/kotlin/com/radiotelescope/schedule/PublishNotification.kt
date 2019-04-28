69ac6c6290a6471b6bfc0554ef84e21a1482ea94
jmajeski <jacobmajeski@gmail.com>
2019-04-28 17:03:00 -0400

    Add cron job to publish to subscribed appointments

---
+++ src/main/kotlin/com/radiotelescope/schedule/PublishNotification.kt
@@ -0,0 +1,55 @@
+package com.radiotelescope.schedule
+
+import com.amazonaws.services.sns.AmazonSNSClientBuilder
+import com.radiotelescope.repository.appointment.IAppointmentRepository
+import com.radiotelescope.repository.log.ILogRepository
+import com.radiotelescope.repository.log.Log
+import com.radiotelescope.repository.subscribedAppointment.ISubscribedAppointmentRepository
+import com.radiotelescope.repository.subscribedAppointment.subscribedAppointment
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class PublishNotification(
+        private val logRepo: ILogRepository,
+        private val subscribedAppointmentRepo: ISubscribedAppointmentRepository,
+        private val appointmentRepo: IAppointmentRepository
+) {
+    @Scheduled(cron = Settings.EVERY_MINUTE)
+    fun execute(){
+        subscribedAppointmentRepo.findAll().forEach{
+            if (appointmentRepo.findById(it.appointmentId).get().startTime.before(Date()) && it.status == subscribedAppointment.Status.SCHEDULED){
+                val builder = AmazonSNSClientBuilder.standard().withRegion("us-east-2").build()
+                val topicARN = builder.createTopic("UserTopic" + it.userId).topicArn
+
+                builder.publish(topicARN, "Your appointment " + it.appointmentId + "is about to begin.")
+                it.status = subscribedAppointment.Status.STARTED
+                subscribedAppointmentRepo.save(it)
+                builder.shutdown()
+                createLogs(it)
+            }
+            else if(appointmentRepo.findById(it.appointmentId).get().endTime.before(Date()) && it.status == subscribedAppointment.Status.STARTED){
+                val builder = AmazonSNSClientBuilder.standard().withRegion("us-east-2").build()
+                val topicARN = builder.createTopic("UserTopic" + it.userId).topicArn
+
+                builder.publish(topicARN, "Your appointment " + it.appointmentId + "has finished.")
+                subscribedAppointmentRepo.deleteById(it.id)
+                builder.shutdown()
+                createLogs(it)
+            }
+        }
+    }
+
+    private fun createLogs(subscribedAppointment: subscribedAppointment) {
+        val notifySubscribedAppointmentLog = Log(
+                affectedTable = Log.AffectedTable.SUBSCRIBED_APPOINTMENT,
+                action = "Notify User of Subscribed Appointment",
+                timestamp = Date(),
+                affectedRecordId = subscribedAppointment.id
+        )
+
+        logRepo.save(notifySubscribedAppointmentLog)
+    }
+
+}
--- src/main/kotlin/com/radiotelescope/schedule/Settings.kt
+++ src/main/kotlin/com/radiotelescope/schedule/Settings.kt
@@ -3,5 +3,6 @@ package com.radiotelescope.schedule
 class Settings {
     companion object {
         const val EVERY_HOUR = "0 0 * * * *"
+        const val EVERY_MINUTE = "0 * * * * *"
     }
 }
