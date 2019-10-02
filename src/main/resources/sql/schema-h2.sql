88a9f7bb963e5b43215f03ab7dbcd61ea2100230
jhorne98 <jhorne@ycp.edu>
2019-10-01 22:42:35 -0400

    Added video-file table to liquibase configuration and startup configuration

--- src/main/resources/sql/schema-h2.sql
+++ src/main/resources/sql/schema-h2.sql
@@ -129,3 +129,12 @@ CREATE TABLE login_attempt (
   user_id INT(11) NOT NULL,
   login_time DATETIME NOT NULL
 );
+
+CREATE TABLE video_file (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    thumbnail_path VARCHAR(100) NOT NULL,
+    video_path VARCHAR(100) NOT NULL,
+    video_length TIME(0) NOT NULL,
+    record_created_timestamp DATETIME NOT NULL,
+    record_updated_timestamp DATETIME NOT NULL
+);
