ce8f3607075c0247dbdc65de7a15e9893e0aeac9
jhorne98 <jhorne@ycp.edu>
2019-11-20 15:20:38 -0500

    Reorganized database changelog for merge.

--- src/main/resources/db/changelog/34-update-video-file-table.sql
+++ src/main/resources/db/changelog/34-update-video-file-table.sql
@@ -1,12 +1,12 @@
 -- liquibase formatted sql
 
--- changeset jhorne:62
+-- changeset jhorne:80
 ALTER TABLE video_file
 MODIFY record_created_timestamp TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6)
 
 -- rollback alter table video_file modify record_created_timestamp DATETIME not null
 
--- changeset jhorne:63
+-- changeset jhorne:81
 ALTER TABLE video_file
 MODIFY record_updated_timestamp TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
 
