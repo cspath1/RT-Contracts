ab91ff4d6a1ad11cc93df61ef5bf97e03829e674
jhorne98 <jhorne@ycp.edu>
2020-01-01 13:01:24 -0500

    Added acceleration table repository

--- src/main/kotlin/com/radiotelescope/repository/temperature/ITemperatureRepository.kt
+++ src/main/kotlin/com/radiotelescope/repository/temperature/ITemperatureRepository.kt
@@ -2,5 +2,8 @@ package com.radiotelescope.repository.temperature
 
 import org.springframework.stereotype.Repository
 
+/**
+ * Spring Repository for the [Temperature] Entity
+ */
 @Repository
 interface ITemperatureRepository
