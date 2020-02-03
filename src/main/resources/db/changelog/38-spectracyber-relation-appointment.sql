92d6ba2663bab4405e850f8ec7e438c86e5a756f
jhorne98 <jhorne@ycp.edu>
2020-02-03 12:29:26 -0500

    Changed spectracyber-relation-id to not null

--- src/main/resources/db/changelog/38-spectracyber-relation-appointment.sql
+++ src/main/resources/db/changelog/38-spectracyber-relation-appointment.sql
@@ -2,6 +2,6 @@
 
 -- changeset jhorne:87
 ALTER TABLE appointment
-ADD spectracyber_config_id INT(11) AFTER orientation_id;
+ADD spectracyber_config_id INT(11) NOT NULL AFTER orientation_id;
 
 -- rollback alter table appointment drop spectracyber_config_id
