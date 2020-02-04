9151a2b464a1b61b426381939b59327cd8507070
jhorne98 <jhorne@ycp.edu>
2020-02-04 11:03:45 -0500

    Added thresholds table.

---
+++ src/main/resources/db/changelog/39-create-sensor-thresholds.sql
@@ -0,0 +1,21 @@
+-- liquibase formatted sql
+
+-- changeset jhorne:88
+CREATE TABLE thresholds (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    wind INT(11) NOT NULL,
+    az_motor_temp INT(11) NOT NULL,
+    elev_motor_temp INT(11) NOT NULL,
+    az_motor_vibration INT(11) NOT NULL,
+    elev_motor_vibration INT(11) NOT NULL,
+    az_motor_current INT(11) NOT NULL,
+    elev_motor_current INT(11) NOT NULL,
+    counter_balance_vibration FLOAT(7, 4) NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+INSERT INTO thresholds(wind, az_motor_temp, elev_motor_temp, az_motor_vibration, elev_motor_vibration, az_motor_current, elev_motor_current, counter_balance_vibration)
+    VALUES(30, 80, 80, 1, 1, 6, 6, 0.42);
+
+-- rollback drop table thresholds
