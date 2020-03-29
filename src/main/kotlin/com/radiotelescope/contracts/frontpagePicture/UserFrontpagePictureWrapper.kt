f1d6a0be19f5bb15542881a83153ccf9789ab81a
jhorne98 <jhorne@ycp.edu>
2020-03-28 20:07:25 -0400

    Added FrontpagePicture wrapper

---
+++ src/main/kotlin/com/radiotelescope/contracts/frontpagePicture/UserFrontpagePictureWrapper.kt
@@ -0,0 +1,34 @@
+package com.radiotelescope.contracts.frontpagePicture
+
+import com.google.common.collect.Multimap
+import com.radiotelescope.contracts.SimpleResult
+import com.radiotelescope.security.UserContext
+import com.radiotelescope.repository.frontpagePicture.FrontpagePicture
+import com.radiotelescope.repository.role.UserRole
+import com.radiotelescope.security.AccessReport
+
+/**
+ * Wrapper that takes a [FrontpagePictureFactory] and is responsible for all
+ * user role validations for the [FrontpagePicture] Entity
+ *
+ * @property context the [UserContext] interface
+ * @property factory the [FrontpagePictureFactory] factory interface
+ */
+class UserFrontpagePictureWrapper (
+        private val context: UserContext,
+        private val factory: FrontpagePictureFactory
+) {
+    /**
+     * Wrapper method for the [FrontpagePictureFactory.submit] method.
+     *
+     * @param request the [Submit.Request] form
+     * @param withAccess anonymous function that uses the command's result object
+     * @return An [AccessReport] if authentication fails, null otherwise
+     */
+    fun submit(request: Submit.Request, withAccess: (result: SimpleResult<FrontpagePicture, Multimap<ErrorTag, String>>) -> Unit): AccessReport? {
+        return context.require(
+                requiredRoles = listOf(UserRole.Role.USER),
+                successCommand = factory.submit(request)
+        ).execute(withAccess)
+    }
+}
