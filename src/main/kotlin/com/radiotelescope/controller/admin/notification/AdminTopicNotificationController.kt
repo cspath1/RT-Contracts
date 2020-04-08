b8b5f254c5052225af6eb9db80925f248a233b1b
jhorne98 <jhorne@ycp.edu>
2020-04-07 22:43:30 -0400

    Added tests to AdminTopicNotificationController. Needs logs and default ARN

--- src/main/kotlin/com/radiotelescope/controller/admin/notification/AdminTopicNotificationController.kt
+++ src/main/kotlin/com/radiotelescope/controller/admin/notification/AdminTopicNotificationController.kt
@@ -3,15 +3,12 @@ package com.radiotelescope.controller.admin.notification
 import com.google.common.collect.HashMultimap
 import com.radiotelescope.controller.BaseRestController
 import com.radiotelescope.controller.model.Result
-import com.radiotelescope.controller.model.appointment.create.CelestialBodyAppointmentCreateForm
-import com.radiotelescope.controller.model.ses.SesSendForm
 import com.radiotelescope.controller.model.sns.SnsSendForm
 import com.radiotelescope.controller.spring.Logger
 import com.radiotelescope.repository.role.IUserRoleRepository
 import com.radiotelescope.repository.role.UserRole
 import com.radiotelescope.security.UserContext
 import com.radiotelescope.service.sns.ErrorTag
-import com.radiotelescope.service.ses.IAwsSesSendService
 import com.radiotelescope.service.sns.IAwsSnsSendService
 import com.radiotelescope.toStringMap
 import org.springframework.http.HttpStatus
@@ -19,23 +16,35 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * REST Controller to handle Admin broadcast notifications
+ *
+ * @param context the [UserContext]
+ * @param roleRepo the [IUserRoleRepository]
+ * @param logger the [Logger] service
+ */
 @RestController
 class AdminTopicNotificationController (
         private val context: UserContext,
         private val roleRepo: IUserRoleRepository,
-        private val awsSesSendService: IAwsSesSendService,
         private val awsSnsSendService: IAwsSnsSendService,
         logger: Logger
 ) : BaseRestController(logger) {
     @PostMapping(value = ["/api/notification/topic"])
-    fun execute(@RequestParam(value = "topic", required = false, defaultValue = "") topic: String,
+    fun execute(@RequestParam(value = "topic", required = false) topic: String?,
                 @RequestParam(value = "message", required = true) message: String
     ) : Result {
+        var sendTopic = topic
+        if(topic.isNullOrBlank()) {
+            sendTopic = ""
+        }
+
         // Test if the user is an admin
-        val isAdmin = (roleRepo.findAllApprovedRolesByUserId(context.currentUserId()!!).find { role -> UserRole.Role.ADMIN == role.role } != null)
+        val isAdmin = if(context.currentUserId() == null) false else (roleRepo.findAllApprovedRolesByUserId(context.currentUserId()!!).find { role -> UserRole.Role.ADMIN == role.role } != null)
         if (isAdmin) {
+            // If admin, send SMS to everyone in the selected topic
             sendSms(
-                    topic = topic,
+                    topic = sendTopic!!,
                     message = message
             )?.let { errors ->
                 result =  if (errors.isEmpty) Result() else Result(errors = errors.toStringMap(), status = HttpStatus.BAD_REQUEST)
@@ -45,17 +54,6 @@ class AdminTopicNotificationController (
         }
 
         return result
-    }
-
-    private fun sendEmail(email: String, form: CelestialBodyAppointmentCreateForm) {
-        val sendForm = SesSendForm(
-                toAddresses = listOf(email),
-                fromAddress = "YCAS Radio Telescope <cspath1@ycp.edu>",
-                subject = "Celestial Body Appointment Created",
-                htmlBody = "<p>Your celestial body appointment has been scheduled to start at ${form.startTime} " +
-                        "and end at ${form.endTime}.</p>"
-        )
-        awsSesSendService.execute(sendForm)
     }
 
     private fun sendSms(topic: String, message: String) :  HashMultimap<ErrorTag, String>?{
---
+++ src/test/kotlin/com/radiotelescope/controller/admin/notification/AdminTopicNotificationControllerTest.kt
@@ -0,0 +1,91 @@
+package com.radiotelescope.controller.admin.notification
+
+import com.radiotelescope.controller.BaseRestControllerTest
+import com.radiotelescope.repository.log.ILogRepository
+import com.radiotelescope.repository.role.IUserRoleRepository
+import com.radiotelescope.repository.role.UserRole
+import com.radiotelescope.repository.user.User
+import com.radiotelescope.services.sns.MockAwsSnsSendService
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit4.SpringRunner
+
+@DataJpaTest
+@RunWith(SpringRunner::class)
+internal class AdminTopicNotificationControllerTest : BaseRestControllerTest() {
+    @Autowired
+    private lateinit var logRepo: ILogRepository
+
+    @Autowired
+    private lateinit var roleRepo: IUserRoleRepository
+
+    private lateinit var adminTopicNotificationController: AdminTopicNotificationController
+
+    private lateinit var user: User
+
+    @Before
+    override fun init() {
+        super.init()
+
+        adminTopicNotificationController = AdminTopicNotificationController(
+                context = getContext(),
+                roleRepo = roleRepo,
+                awsSnsSendService = MockAwsSnsSendService(true),
+                logger = getLogger()
+        )
+
+        user = testUtil.createUser("jhorne@ycp.edu")
+    }
+
+    @Test
+    fun testSuccessResponseDefaultTopic() {
+        getContext().login(user.id)
+        // Make user an admin
+        getContext().currentRoles.add(UserRole.Role.ADMIN)
+        testUtil.createUserRoleForUser(user, UserRole.Role.ADMIN, true)
+
+        val result = adminTopicNotificationController.execute(
+                topic = null,
+                message = "Test Message"
+        )
+
+        assertNotNull(result)
+        assertEquals(HttpStatus.OK, result.status)
+        assertNull(result.errors)
+    }
+
+    @Test
+    fun testSuccessResponseOtherTopic() {
+        getContext().login(user.id)
+        // Make user an admin
+        getContext().currentRoles.add(UserRole.Role.ADMIN)
+        testUtil.createUserRoleForUser(user, UserRole.Role.ADMIN, true)
+
+        val result = adminTopicNotificationController.execute(
+                topic = "testARN",
+                message = "Test Message"
+        )
+
+        assertNotNull(result)
+        assertEquals(HttpStatus.OK, result.status)
+        assertNull(result.errors)
+    }
+
+    @Test
+    fun testFailedAuthenticationResponse() {
+        // Do not log the user in
+        val result = adminTopicNotificationController.execute(
+                topic = "testARN",
+                message = "Test Message"
+        )
+
+        assertNotNull(result)
+        assertEquals(HttpStatus.FORBIDDEN, result.status)
+        assertNull(result.errors)
+    }
+}
