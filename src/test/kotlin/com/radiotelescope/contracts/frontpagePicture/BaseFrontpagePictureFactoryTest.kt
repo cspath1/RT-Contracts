000df03ebe7d31effb09fe3a1f5d266bfacfbcec
jhorne98 <jhorne@ycp.edu>
2020-04-24 20:03:14 -0400

    Added frontpage picture test

--- src/test/kotlin/com/radiotelescope/contracts/frontpagePicture/BaseFrontpagePictureFactoryTest.kt
+++ src/test/kotlin/com/radiotelescope/contracts/frontpagePicture/BaseFrontpagePictureFactoryTest.kt
@@ -3,7 +3,7 @@ package com.radiotelescope.contracts.frontpagePicture
 import com.radiotelescope.AbstractSpringTest
 import com.radiotelescope.repository.frontpagePicture.IFrontpagePictureRepository
 import com.radiotelescope.services.s3.MockAwsS3DeleteService
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -40,6 +40,38 @@ internal class BaseFrontpagePictureFactoryTest : AbstractSpringTest() {
         )
 
         // Ensure it is the correct command
-        Assert.assertTrue(cmd is Submit)
+        assertTrue(cmd is Submit)
+    }
+
+    @Test
+    fun retrieveList() {
+        // Call the factory method
+        val cmd = factory.retrieveList()
+
+        // Ensure it is the correct command
+        assertTrue(cmd is RetrieveList)
+    }
+
+    @Test
+    fun retrieveApproved() {
+        // Call the factory method
+        val cmd = factory.retrieveApproved()
+
+        // Ensure it is the correct command
+        assertTrue(cmd is RetrieveApproved)
+    }
+
+    @Test
+    fun approveDeny() {
+        // Call the factory method
+        val cmd = factory.approveDeny(
+                request = ApproveDeny.Request(
+                        frontpagePictureId = 1L,
+                        isApprove = true
+                )
+        )
+
+        // Call the factory method
+        assertTrue(cmd is ApproveDeny)
     }
 }
