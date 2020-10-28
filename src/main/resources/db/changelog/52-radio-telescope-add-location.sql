3280305054fb1c0c6efc5a38dad5713e26d9fa64
David Nelson <supermariokart98@gmail.com>
2020-10-27 20:35:49 -0400

    Added foreign key in radio_telescope for location
    Issue #80

--- src/main/resources/db/changelog/52-radio-telescope-add-location.sql
+++ src/main/resources/db/changelog/52-radio-telescope-add-location.sql
@@ -2,7 +2,9 @@
 
 -- changeset pnelson:106
 ALTER TABLE radio_telescope
-ADD location_id int(11)
+ADD location_id int
 DEFAULT 0;
 
+ALTER TABLE radio_telescope
+ADD FOREIGN KEY(location_id) REFERENCES location(id);
 -- rollback ALTER TABLE radio_telescope DROP COLUMN location;
