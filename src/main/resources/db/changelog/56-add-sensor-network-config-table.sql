6545a4187265eb181bd7c0055708636b29ce1515
Patrick <pnelson1@ycp.edu>
2021-03-19 15:53:00 -0400

    Fixed mistake where column was nullable when we want it non-nullable
    Issue #121

--- src/main/resources/db/changelog/56-add-sensor-network-config-table.sql
+++ src/main/resources/db/changelog/56-add-sensor-network-config-table.sql
@@ -6,7 +6,7 @@ CREATE TABLE sensor_network_config (
   telescope_id INT(11) NOT NULL,
   sensor_initialization INT(11) NOT NULL,
   timeout_data_retrieval INT(11) NOT NULL,
-  timeout_initialization INT(11),
+  timeout_initialization INT(11) NOT NULL,
   insert_timestamp TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6),
   update_timestamp TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
 
