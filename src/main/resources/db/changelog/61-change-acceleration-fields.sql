1d085022db9e0c1a0f787fce866d36a6870f2d86
davidmchugh2000 <dmchugh@ycp.edu>
2021-04-26 18:58:54 -0400

    Rllback now float(4,3)
    Rollback changed from int to float(4,3)

--- src/main/resources/db/changelog/61-change-acceleration-fields.sql
+++ src/main/resources/db/changelog/61-change-acceleration-fields.sql
@@ -9,7 +9,7 @@ ALTER TABLE acceleration
           MODIFY acceleration_z int;
 
 -- rollback ALTER TABLE acceleration
--- MODIFY acceleration_magnitude int,
--- MODIFY acceleration_x int,
--- MODIFY acceleration_y int,
--- MODIFY acceleration_z int;
+-- MODIFY acceleration_magnitude float(4,3),
+-- MODIFY acceleration_x float(4,3),
+-- MODIFY acceleration_y float(4,3),
+-- MODIFY acceleration_z float(4,3);
