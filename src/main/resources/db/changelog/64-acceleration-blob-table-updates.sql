d1ff640c429c9db3f292f24efcb149c323ffb234
Jack Anderson <60368246+andersonjackc@users.noreply.github.com>
2021-10-26 12:21:35 -0400

    Remove old Blob table, added new blob tables

---
+++ src/main/resources/db/changelog/64-acceleration-blob-table-updates.sql
@@ -0,0 +1,33 @@
+-- liquibase formatted sql
+
+-- changeset dmchugh:119
+
+DROP TABLE acceleration_blob;
+
+CREATE TABLE azimuth_acceleration_blob (
+                                   id INT(11) NOT NULL AUTO_INCREMENT,
+                                   acc_blob BLOB DEFAULT NULL,
+                                   first_time_captured BIGINT(20) DEFAULT NULL,
+
+                                   PRIMARY KEY (id)
+);
+
+CREATE TABLE elevation_acceleration_blob (
+                                    id INT(11) NOT NULL AUTO_INCREMENT,
+                                    acc_blob BLOB DEFAULT NULL,
+                                    first_time_captured BIGINT(20) DEFAULT NULL,
+
+                                    PRIMARY KEY (id)
+);
+
+CREATE TABLE counterbalance_acceleration_blob (
+                                    id INT(11) NOT NULL AUTO_INCREMENT,
+                                    acc_blob BLOB DEFAULT NULL,
+                                    first_time_captured BIGINT(20) DEFAULT NULL,
+
+                                    PRIMARY KEY (id)
+);
+
+-- rollback drop table azimuth_acceleration_blob;
+-- rollback drop table elevation_acceleration_blob;
+-- rollback drop table counterbalance_acceleration_blob;
