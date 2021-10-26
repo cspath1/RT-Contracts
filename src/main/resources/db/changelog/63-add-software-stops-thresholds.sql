9216c7ecbfb052e8709716da2682adb1494f49ad
Tyler <tfranks@ycp.edu>
2021-10-26 19:46:07 -0400

    Made thresholds NOT NULL to fix CR null issues

--- src/main/resources/db/changelog/63-add-software-stops-thresholds.sql
+++ src/main/resources/db/changelog/63-add-software-stops-thresholds.sql
@@ -3,8 +3,8 @@
 -- changeset tfranks:118
 
 ALTER TABLE radio_telescope
-    ADD elevation_max_degree float(5,2),
-    ADD elevation_min_degree float(5,2);
+    ADD elevation_max_degree float(5,2) NOT NULL,
+    ADD elevation_min_degree float(5,2) NOT NULL;
 
 -- rollback ALTER TABLE radio_telescope
 -- DROP COLUMN elevation_max_degree,
