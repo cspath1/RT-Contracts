f1db4529315d4f5b3ed7717e6437d513aca31dcb
David McHugh <dmchugh@ycp.edu>
2022-05-08 14:44:56 -0400

    Cahnged for API call

--- src/main/kotlin/com/radiotelescope/repository/sensorStatus/SensorStatus.kt
+++ src/main/kotlin/com/radiotelescope/repository/sensorStatus/SensorStatus.kt
@@ -14,14 +14,31 @@ import javax.persistence.*
 data class SensorStatus(
         @Column(name = "gate", nullable = false)
         var gate: Int?,
-        @Column(name = "proximity", nullable = false)
-        var proximity: Int?,
-        @Column(name = "azimuth_motor", nullable = false)
-        var azimuthMotor: Int?,
-        @Column(name = "elevation_motor", nullable = false)
-        var elevationMotor: Int?,
         @Column(name = "weather_station", nullable = false)
-        var weatherStation: Int?
+        var weatherStation: Int?,
+        @Column(name = "elevation_abs_encoder ", nullable = false)
+        var elevation_abs_encoder : Int?,
+        @Column(name = "azimuth_abs_encoder ", nullable = false)
+        var azimuth_abs_encoder : Int?,
+        @Column(name = "el_proximity_0  ", nullable = false)
+        var el_proximity_0  : Int?,
+        @Column(name = "el_proximity_90  ", nullable = false)
+        var el_proximity_90  : Int?,
+        @Column(name = "az_motor_temp_1  ", nullable = false)
+        var az_motor_temp_1  : Int?,
+        @Column(name = "az_motor_temp_2 ", nullable = false)
+        var az_motor_temp_2 : Int?,
+        @Column(name = "el_motor_temp_1  ", nullable = false)
+        var el_motor_temp_1  : Int?,
+        @Column(name = "el_motor_temp_2  ", nullable = false)
+        var el_motor_temp_2  : Int?,
+        @Column(name = "az_accel  ", nullable = false)
+        var az_accel  : Int?,
+        @Column(name = "el_accel   ", nullable = false)
+        var el_accel   : Int?,
+        @Column(name = "counter_balance_accel   ", nullable = false)
+        var counter_balance_accel   : Int?
+
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
